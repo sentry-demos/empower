5182edfa8b13e04125856e50ae72295bae53cc2a
stevenplewis <steven@notsteve.com>
2021-04-21 17:02:43 -0700

    slightly more humorous 404 copy

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -6,9 +6,13 @@ function NotFound() {
     <div className="checkout-container-complete">
       <h2>404</h2>
       <p>
-        We can't seem to find the page you were just directed to. Please go back
-        in your browser or go <Link to="/">back to our home page</Link> and
-        accept our deepest apologies.
+        The page you're looking for can't be found and we can't get to the root
+        of it just yet. Please go back in your browser or go{' '}
+        <Link to="/">back to our home page</Link> and accept our deepest
+        apologies.
+      </p>
+      <p>
+        If the issue persists, please <Link to="/">contact us</Link>.
       </p>
     </div>
   );
