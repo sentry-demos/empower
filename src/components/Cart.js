4bc87375ee643ab68e891fabf712ad94b20efa5b
Cameron McEfee <cm@cameronmcefee.com>
2021-04-30 09:46:40 -0700

    convert cart to class component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,60 +1,63 @@
-import { useContext } from 'react';
+import { Component } from 'react';
 import Context from '../utils/context';
 import { Link } from 'react-router-dom';
 import './cart.css';
 
 import Button from './ButtonLink';
 
-function Checkout(props) {
-  const { cart } = useContext(Context);
-  return (
-    <div className="cart-container">
-      <h2>Cart</h2>
-      {cart.items.length > 0 ? (
-        <>
-          <ul className="cart-list">
-            {cart.items.map((item) => {
-              const quantity = cart.quantities[item.id];
-              const itemLink = '/product/' + item.id;
-              return (
-                <li className="cart-item" key={item.id}>
-                  <Link to={itemLink}>
-                    <img src={item.img} alt="item-thumbnail" />
-                  </Link>
-                  <Link to={itemLink}>
-                    <h4>{item.title}</h4>
-                  </Link>
-                  <p>${item.price}.00</p>
-                  <div className="quantity-adjust">
-                    <button
-                      onClick={() =>
-                        cart.update({ action: 'remove', product: item })
-                      }
-                    >
-                      –
-                    </button>
-                    <span>{quantity}</span>
-                    <button
-                      onClick={() =>
-                        cart.update({ action: 'add', product: item })
-                      }
-                    >
-                      +
-                    </button>
-                  </div>
-                  <p>${item.price * quantity}.00</p>
-                </li>
-              );
-            })}
-          </ul>
-          <h3 className="cart-subtotal">Cart Subtotal: ${cart.total}.00</h3>
-          <Button to="/checkout">Proceed to checkout</Button>
-        </>
-      ) : (
-        <p>Please add items to the cart</p>
-      )}
-    </div>
-  );
+class Cart extends Component {
+  static contextType = Context;
+  render() {
+    const { cart } = this.context;
+    return (
+      <div className="cart-container">
+        <h2>Cart</h2>
+        {cart.items.length > 0 ? (
+          <>
+            <ul className="cart-list">
+              {cart.items.map((item) => {
+                const quantity = cart.quantities[item.id];
+                const itemLink = '/product/' + item.id;
+                return (
+                  <li className="cart-item" key={item.id}>
+                    <Link to={itemLink}>
+                      <img src={item.img} alt="item-thumbnail" />
+                    </Link>
+                    <Link to={itemLink}>
+                      <h4>{item.title}</h4>
+                    </Link>
+                    <p>${item.price}.00</p>
+                    <div className="quantity-adjust">
+                      <button
+                        onClick={() =>
+                          cart.update({ action: 'remove', product: item })
+                        }
+                      >
+                        –
+                      </button>
+                      <span>{quantity}</span>
+                      <button
+                        onClick={() =>
+                          cart.update({ action: 'add', product: item })
+                        }
+                      >
+                        +
+                      </button>
+                    </div>
+                    <p>${item.price * quantity}.00</p>
+                  </li>
+                );
+              })}
+            </ul>
+            <h3 className="cart-subtotal">Cart Subtotal: ${cart.total}.00</h3>
+            <Button to="/checkout">Proceed to checkout</Button>
+          </>
+        ) : (
+          <p>Please add items to the cart</p>
+        )}
+      </div>
+    );
+  }
 }
 
-export default Checkout;
+export default Cart;
