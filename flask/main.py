75281ed752924fcc419fb56d4ad88d6ce8fed02d
William Capozzoli <thinkocapo@gmail.com>
2021-05-02 11:18:01 -0700

    requires python3

--- flask/main.py
+++ flask/main.py
@@ -1,10 +1,8 @@
-# TODO build application-monitoring Empower Plant backend
-# TODO add app.yaml here and application-monitoring-flask service. application-monitoring can be react.
-
+import sys
 # import os
 # from flask import Flask, request, json, abort, make_response, jsonify
 from flask import Flask
-# from flask_cors import CORS
+from flask_cors import CORS
 # from dotenv import load_dotenv
 # from dotenv import load_dotenv
 # import sentry_sdk
@@ -28,15 +26,29 @@ from flask import Flask
 # )
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
 @app.route('/success', methods=['GET'])
 def success():    
-    print('/success')
-    # response = make_response("success")
-    return "success"
+    # print('/successs')
+    # response = make_response("successs")
+    return "successs"
+
+@app.route('/products', methods=['GET'])
+def products():    
+    # print('/products')
+    # response = make_response("productss")
+    return "products"
 
 if __name__ == '__main__':
+    i = sys.version_info
+    print(i.major)
+    # if i.major != "3":
+    if sys.version_info[0] < 3:
+        raise SystemExit("Failed to start: need python3")
+        # sys.exit()
+    # else:
+        # print("it is", typeof(i.major))
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app.
     app.run(host='127.0.0.1', port=8080, debug=True)
