383c433c7ff4712502e1d9a77cc00b0b768dea36
William Capozzoli <thinkocapo@gmail.com>
2021-05-28 21:43:09 -0700

    release generator

--- flask/utils.py
+++ flask/utils.py
@@ -1,10 +1,11 @@
-## TODO util or rm this altogether, require always .env or shell script defined? TBD
-# RELEASE = ""
-# if os.environ.get("RELEASE") is None:
-#     d=datetime.date.today()
-#     week=str((d.day-1)//7+1)
-#     date_given = datetime.datetime.today().date()
-#     month = str(date_given.month)
-#     RELEASE = month + "." + week
-# else:
-#     RELEASE = os.environ.get("RELEASE")
+from datetime import datetime
+import numpy
+from pytz import timezone
+import time
+
+def release():
+    d=datetime.today()
+    week=str((d.day-1)//7+1)
+    date_given = datetime.today().date()
+    month = str(date_given.month)
+    return month + "." + week
