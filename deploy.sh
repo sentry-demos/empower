bc9076e709a1bce6383d8057abcde5375482f2a4
William Capozzoli <thinkocapo@gmail.com>
2021-05-30 11:16:52 -0700

    updates to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,20 @@
-day=$(date +%d)
-month=$(date +%-m)
-if [ "$day" -ge 0 ] && [ "$day" -le 7 ]; then
-  week=1
-elif [ "$day" -ge 8 ] &&  [ "$day" -le 14 ]; then
-  week=2
-elif [ "$day" -ge 15 ] &&  [ "$day" -le 21 ]; then
-  week=3
-elif [ "$day" -ge 22 ]; then
-  week=4
-fi
-RELEASE="$month.$week"
+RELEASE=`./release.sh`
 echo $RELEASE
 
-SENTRY_ORG=testorg-az
-SENTRY_PROJECT=javascript-react
+SENTRY_ORG=will-captel
+SENTRY_PROJECT=application-monitoring-javascript
 PREFIX=static/js
 REPOSITORY=us.gcr.io/sales-engineering-sf
 
+npm run build
+
 sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $RELEASE
-sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $RELEASE
-sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT files $RELEASE upload-sourcemaps --url-prefix "~/static/js" --validate react/build/$PREFIX
+# sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $RELEASE
+sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT files $RELEASE upload-sourcemaps --url-prefix "~/static/js" --validate build/$PREFIX
 
-# RELEASE was already baked into the prod build, so no need to `--update-env-vars` it for the React app
-npm run build && gcloud app deploy
-echo 'REACT DEPLOY DONE'
+# The release was set in the static prod build
+gcloud app deploy
 
-
-# `gcloud app deploy` does not support `--update-env-vars RELEASE=$RELEASE`
+# # `gcloud app deploy` does not support `--update-env-vars RELEASE=$RELEASE`
 # cd flask && gcloud app deploy 
-echo 'FLASK DEPLOY DONE'
+# echo 'FLASK DEPLOY DONE'
