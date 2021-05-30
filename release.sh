0fc6d83691d615cc8e3c5463ea7093cdb748c611
William Capozzoli <thinkocapo@gmail.com>
2021-05-29 21:42:25 -0700

    calendar versioning release

---
+++ release.sh
@@ -0,0 +1,13 @@
+# Sets a release according to Calendar Versioning. Month, followed by week of the month.
+day=$(date +%d)
+month=$(date +%-m)
+if [ "$day" -ge 0 ] && [ "$day" -le 7 ]; then
+  week=1
+elif [ "$day" -ge 8 ] &&  [ "$day" -le 14 ]; then
+  week=2
+elif [ "$day" -ge 15 ] &&  [ "$day" -le 21 ]; then
+  week=3
+elif [ "$day" -ge 22 ]; then
+  week=4
+fi
+echo "$month.$week"
