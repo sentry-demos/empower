e3f9a77b27fcd2c2177c2d45c51248f1c8709779
William Capozzoli <thinkocapo@gmail.com>
2021-06-09 08:18:17 -0700

    purpose

--- release.sh
+++ release.sh
@@ -1,4 +1,6 @@
 # Sets a release according to Calendar Versioning. Month, followed by week of the month.
+# In sentry-demos/tracing, we did not manage this whatsoever
+# This release.sh is re-used in many places so that we don't need 3 different Makefiles like in sentry-demos/tracing
 day=$(date +%d)
 month=$(date +%-m)
 if [ "$day" -ge 0 ] && [ "$day" -le 7 ]; then
