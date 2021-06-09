2a105765a412d113066a028603d775a22210682d
William Capozzoli <thinkocapo@gmail.com>
2021-06-09 08:39:26 -0700

    react local run script

---
+++ run.sh
@@ -0,0 +1,14 @@
+RELEASE=`./release.sh`
+echo $RELEASE
+
+SENTRY_ORG=testorg-az
+SENTRY_PROJECT=application-monitoring-javascript
+PREFIX=static/js
+
+npm run build
+
+sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $RELEASE
+sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $RELEASE
+sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT files $RELEASE upload-sourcemaps --url-prefix "~/static/js" --validate build/$PREFIX
+
+serve -s build
