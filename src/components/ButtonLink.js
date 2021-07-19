b5d989769b131b71168687bbed8f0afdb792f89e
William Capozzoli <thinkocapo@gmail.com>
2021-07-19 14:11:58 -0700

    props Button style visibility

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class Button extends Component {
   render() {
     return (
-      <Link {...this.props} className="btn">
+      <Link {...this.props} className="btn" style={{visibility: this.props.visibility}}>
         {this.props.children}
       </Link>
     );
