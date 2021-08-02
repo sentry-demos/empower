08d132d8acd3e3cff42de43dd136e33efb970e89
William Capozzoli <thinkocapo@gmail.com>
2021-08-02 15:05:18 -0700

    clean

--- flask/db.py
+++ flask/db.py
@@ -41,10 +41,8 @@ def get_products():
             connection = db.connect()
 
         with sentry_sdk.start_span(op="get_products", description="db.query") as span:
-            # WARNING if you do pg_sleep(2) then it's not 2 seconds, it's somehow like 5 - 10 seconds...
-            # WARNING if you do pg_sleep(3) then it's not 3 seconds, it's somehow like 10 - 20 seconds...
             n = weighter(operator.le, 12)
-            print("> n",n)
+
             products = connection.execute(
                 "SELECT *, pg_sleep(%s) FROM products" % (n)
             ).fetchall()
