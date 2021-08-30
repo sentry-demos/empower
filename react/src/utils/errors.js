bfd2318e50545c2736097b2310fb51e320253873
Chris Stavitsky <cstavitsky@Chriss-MacBook-Pro.local>
2021-08-30 16:29:35 -0700

    Throw random error on forced crash

--- react/src/utils/errors.js
+++ react/src/utils/errors.js
@@ -1,22 +1,49 @@
 import { createBrowserHistory } from 'history';
 const history = createBrowserHistory();
 
+// ERRORS
+const notAFunctionError = () => {
+  const someArray = [{ func: function () {}}];
+  someArray[1].func();
+}
+const uriError = () => decodeURIComponent('%');
+const syntaxError = () => eval('foo bar');
+const rangeError = () => {
+  throw new RangeError('Parameter must be between 1 and 100');
+}
+const unhandledError = () => {
+  throw new UnhandledException('unhandled error')
+}
+
+const randomErrors = [
+  notAFunctionError,
+  uriError,
+  syntaxError,
+  rangeError,
+  unhandledError
+]
+
+const throwRandomError = () => {
+  const randomNum = parseInt(Math.random()*randomErrors.length)
+  randomErrors[randomNum]()
+}
+
 var probability = function(n) {
   return !!n && Math.random() <= n;
 };
 
 const crasher = () => {
-  let queryParams = new URLSearchParams(history.location.search)
+  const queryParams = new URLSearchParams(history.location.search)
   if (queryParams !== "") {
-      let crash = queryParams.get("crash")
-      if (crash) {
-            console.log("> crash", crash)
-            if (crash === "true" || probability(parseFloat(crash))) {
-              throw new UnhandledException('unhandled error')
-            }
+    const crash = queryParams.get("crash")
+    if (crash) {
+      console.log("> crash", crash)
+      if (crash === "true" || probability(parseFloat(crash))) {
+        throwRandomError();
       }
+    }
   } else {
-      console.log("> queryParam was", queryParams)
+    console.log("> queryParam was", queryParams)
   }
 }
 
