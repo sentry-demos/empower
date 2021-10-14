5eafbbb945a09643e1cf7c241189bf07327e0811
Neil Manvar <neil@sentry.io>
2021-10-13 19:12:59 -0700

    Got android test written. rename files + remove mobile browser from conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,34 +24,6 @@ sentry_sdk.init(
     environment=ENVIRONMENT,
 )
 
-emusim_browsers = [
-    {
-        "deviceName": "iPhone X Simulator",
-        "browserName": "Safari",
-        "deviceOrientation": "portrait",
-        "platformVersion": "13.4",
-        "platformName": "iOS"
-    }, {
-        "deviceName": "iPhone 11 Simulator",
-        "browserName": "Safari",
-        "deviceOrientation": "portrait",
-        "platformVersion": "13.4",
-        "platformName": "iOS"
-    }, {
-        "deviceName": "Google Pixel 3 XL GoogleAPI Emulator",
-        "browserName": "Chrome",
-        "deviceOrientation": "portrait",
-        "platformVersion": "10.0",
-        "platformName": "Android"
-    },{
-        "deviceName": "Samsung Galaxy S9 WQHD GoogleAPI Emulator",
-        "browserName": "Chrome",
-        "deviceOrientation": "portrait",
-        "platformVersion": "9.0",
-        "platformName": "Android"
-    }]
-
-
 desktop_browsers = [
     {
         "platformName": "Windows 10",
@@ -77,9 +49,7 @@ desktop_browsers = [
         "platformName": "OS X 10.14",
         "browserName": "chrome",
         "platformVersion": "latest",
-        "sauce:options": {
-            "extendedDebugging": True
-        }
+        "sauce:options": {}
     }]
 
 
@@ -92,52 +62,11 @@ def data_center(request):
     return request.config.getoption('--dc')
 
 
-@pytest.fixture(params=emusim_browsers)
-def mobile_web_driver(request, data_center):
-
-    test_name = request.node.name
-    build_tag = environ.get('BUILD_TAG', "Sauce-Best-Practices-Python-Mobile-Web")
-
-    username = environ['SAUCE_USERNAME']
-    access_key = environ['SAUCE_ACCESS_KEY']
-
-    if data_center and data_center.lower() == 'eu':
-        selenium_endpoint = "https://{}:{}@ondemand.eu-central-1.saucelabs.com/wd/hub".format(username, access_key)
-    else:
-        selenium_endpoint = "https://{}:{}@ondemand.us-west-1.saucelabs.com/wd/hub".format(username, access_key)
-
-    caps = dict()
-    caps.update(request.param)
-    caps.update({'build': build_tag})
-    caps.update({'name': test_name})
-
-    browser = webdriver.Remote(
-        command_executor=selenium_endpoint,
-        desired_capabilities=caps,
-        keep_alive=True
-    )
-
-    # This is specifically for SauceLabs plugin.
-    # In case test fails after selenium session creation having this here will help track it down.
-    if browser is not None:
-        print("SauceOnDemandSessionID={} job-name={}".format(browser.session_id, test_name))
-    else:
-        raise WebDriverException("Never created!")
-
-    yield browser
-
-    # Teardown starts here
-    # report results
-    # use the test result to send the pass/fail status to Sauce Labs
-    sauce_result = "failed" if request.node.rep_call.failed else "passed"
-    browser.execute_script("sauce:job-result={}".format(sauce_result))
-    browser.quit()
-
 @pytest.fixture(params=desktop_browsers)
 def desktop_web_driver(request, data_center):
 
     test_name = request.node.name
-    build_tag = environ.get('BUILD_TAG', "Sauce-Best-Practices-Python-Desktop-Web")
+    build_tag = environ.get('BUILD_TAG', "Application-Monitoring-TDA")
 
     username = environ['SAUCE_USERNAME']
     access_key = environ['SAUCE_ACCESS_KEY']
@@ -180,7 +109,7 @@ def desktop_web_driver(request, data_center):
 
 
 @pytest.fixture
-def android_rdc_driver(request, data_center):
+def android_emu_driver(request, data_center):
 
     username_cap = environ['SAUCE_USERNAME']
     access_key_cap = environ['SAUCE_ACCESS_KEY']
@@ -191,9 +120,7 @@ def android_rdc_driver(request, data_center):
         'deviceName': 'Android GoogleAPI Emulator',
         'platformVersion': '10.0',
         'platformName': 'Android',
-        # 'build': 'RDC-Android-Python-Best-Practice',
-        # 'name': request.node.name,
-        'app': "https://github.com/saucelabs/sample-app-mobile/releases/download/2.7.1/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk",
+        'app': "storage:filename=app-release.apk",
         'sauce:options': {
             'appiumVersion': '1.20.2',
             'build': 'RDC-Android-Python-Best-Practice',
@@ -203,18 +130,19 @@ def android_rdc_driver(request, data_center):
     }
 
     if data_center and data_center.lower() == 'eu':
-        sauce_url = 'http://ondemand.eu-central-1.saucelabs.com/wd/hub'
+        sauce_url = "https://{}:{}@ondemand.eu-central-1.saucelabs.com/wd/hub".format(username_cap, access_key_cap)
     else:
-        sauce_url = 'http://ondemand.us-west-1.saucelabs.com/wd/hub'
+        sauce_url = "https://{}:{}@ondemand.us-west-1.saucelabs.com/wd/hub".format(username_cap, access_key_cap)
 
     driver = appiumdriver.Remote(sauce_url, desired_capabilities=caps)
+    driver.implicitly_wait(20)
     yield driver
     sauce_result = "failed" if request.node.rep_call.failed else "passed"
     driver.execute_script("sauce:job-result={}".format(sauce_result))
     driver.quit()
 
 @pytest.fixture
-def ios_rdc_driver(request, data_center):
+def ios_sim_driver(request, data_center):
 
     username_cap = environ['SAUCE_USERNAME']
     access_key_cap = environ['SAUCE_ACCESS_KEY']
@@ -231,15 +159,16 @@ def ios_rdc_driver(request, data_center):
             'build': 'RDC-iOS-Python-Best-Practice',
             'name': request.node.name,
         },
-        'appium:app': 'https://github.com/saucelabs/sample-app-mobile/releases/download/2.7.1/iOS.Simulator.SauceLabs.Mobile.Sample.app.2.7.1.zip',
+        'appium:app': 'https://github.com/sentry-demos/sentry_react_native/releases/download/1.8/sentry_react_native.app.zip',
     }
 
     if data_center and data_center.lower() == 'eu':
-        sauce_url = "http://ondemand.eu-central-1.saucelabs.com/wd/hub"
+        sauce_url = "https://{}:{}@ondemand.eu-central-1.saucelabs.com/wd/hub".format(username_cap, access_key_cap)
     else:
-        sauce_url = "http://ondemand.us-west-1.saucelabs.com/wd/hub"
+        sauce_url = "https://{}:{}@ondemand.us-west-1.saucelabs.com/wd/hub".format(username_cap, access_key_cap)
 
     driver = appiumdriver.Remote(sauce_url, desired_capabilities=caps)
+    driver.implicitly_wait(20)
     yield driver
     sauce_result = "failed" if request.node.rep_call.failed else "passed"
     driver.execute_script("sauce:job-result={}".format(sauce_result))
---
+++ tests/mobile_native/android/test_checkout_android.py
@@ -0,0 +1,20 @@
+def test_standard_user(android_emu_driver):
+
+    # android_emu_driver.find_element_by_class("android.widget.TextView").click() # empower plant btn
+
+    android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[1]/android.widget.TextView').click() # empower plant btn
+
+    add_to_cart_btn = android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.TextView')
+    add_to_cart_btn.click()
+    add_to_cart_btn.click()
+    add_to_cart_btn.click()
+
+    android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.Button[2]/android.widget.TextView').click()
+
+    android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.Button[2]/android.widget.TextView').click()
+
+    android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.EditText').click()
+
+    ## TODO click 'checkout/place order' , can't scroll to it in android
+
+
