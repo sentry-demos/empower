a4e82e923e1a7c334dcb6bbe53b16fc5445f3df5
William Capozzoli <thinkocapo@gmail.com>
2021-12-06 10:50:55 -0800

    update

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $RELEASE
 sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $RELEASE
 sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT files $RELEASE upload-sourcemaps --url-prefix "~/static/js" --validate build/$PREFIX
 
-# The release was set in the static prod build
+# This deploys React - The release was set in the static prod build
 gcloud app deploy --quiet
 
 cd ../flask && gcloud app deploy --quiet
