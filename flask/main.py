626221b81181cb632b5f54a50a33ae669a6cb2e8
William Capozzoli <thinkocapo@gmail.com>
2021-12-10 14:13:43 -0800

    hit right backend

--- flask/main.py
+++ flask/main.py
@@ -84,7 +84,7 @@ def checkout():
  
 @app.route('/success', methods=['GET'])
 def success():    
-    return "successs"
+    return "successs from flask"
 
 @app.route('/products', methods=['GET'])
 def products():    
