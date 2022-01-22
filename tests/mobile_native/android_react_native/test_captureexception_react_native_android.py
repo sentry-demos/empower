371ab5a018a0ffafc4d5fbfdc970d61a17da3066
William Capozzoli <thinkocapo@gmail.com>
2022-01-21 16:42:09 -0800

    comments

--- tests/mobile_native/android_react_native/test_captureexception_react_native_android.py
+++ tests/mobile_native/android_react_native/test_captureexception_react_native_android.py
@@ -1,5 +1,8 @@
 import random
 
+# Handled Exception
+# This error type does not increment the Crash Count for the release, 
+# but the UI in Release dashboard separates from Handled vs Handled, so good to capture some of these, for having a more diverse data set.
 def test_captureexception_react_native_android(android_react_native_emu_driver):
     btn = android_react_native_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[5]/android.widget.TextView')
     btn.click()
