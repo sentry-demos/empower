39b295ec49c83816288243ca4121f5569d24dcae
William Capozzoli <thinkocapo@gmail.com>
2022-01-25 10:53:52 -0800

    rm

--- tests/mobile_native/android_react_native/test_unhandledpromiserejection_react_native_android.py
+++ tests/mobile_native/android_react_native/test_unhandledpromiserejection_react_native_android.py
@@ -1,5 +1,3 @@
-import random
-
 def test_unhandledpromiserejection_react_native_android(android_react_native_emu_driver):
     btn = android_react_native_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[9]/android.widget.TextView')
     btn.click()
