71ccb08b1889941102b8b89f9ef79f90916f0327
William Capozzoli <thinkocapo@gmail.com>
2022-01-27 15:28:45 -0800

    started tests

---
+++ tests/mobile_native/android/test_handlederror_android.py
@@ -0,0 +1,7 @@
+# Clicks the Handled Error button that says ArrayIndexOutOfBoundsException
+def test_handlederror_android(android_emu_driver):
+    btn = android_emu_driver.find_element_by_accessibility_id('').click()
+    btn.click()
+    
+    # add_to_cart_btn = android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.Button')
+    # add_to_cart_btn.click()
