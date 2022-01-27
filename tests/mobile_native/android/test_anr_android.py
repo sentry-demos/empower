00cd9c59bfc55ef7baae0005f2dcca70f122ed43
William Capozzoli <thinkocapo@gmail.com>
2022-01-27 15:51:19 -0800

    added xpaths for all new tests. needs testing

--- tests/mobile_native/android/test_anr_android.py
+++ tests/mobile_native/android/test_anr_android.py
@@ -1,7 +1,4 @@
 # Application Not Responding button
 def test_anr_android(android_emu_driver):
-    btn = android_emu_driver.find_element_by_accessibility_id('').click()
+    btn = android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[4]')
     btn.click()
-    
-    # add_to_cart_btn = android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.Button')
-    # add_to_cart_btn.click()
