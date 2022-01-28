90edeeb6ac46e4984bd3006bb2db563aa7ce6116
William Capozzoli <thinkocapo@gmail.com>
2022-01-28 15:20:08 -0800

    skip markers

--- tests/mobile_native/android/test_nativecrash_android.py
+++ tests/mobile_native/android/test_nativecrash_android.py
@@ -1,6 +1,7 @@
 import time
-
+import pytest
 # NDK/C++ Native Crash SIGSEGV button
+@pytest.mark.skip(reason="not working")
 def test_nativecrash_android(android_emu_driver):
     btn = android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[4]')
     btn.click()
