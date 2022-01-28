af028d2b25ba4dea1b718f9a5a6be97848a6a463
William Capozzoli <thinkocapo@gmail.com>
2022-01-28 15:08:03 -0800

    note on non-working status. todo

--- tests/mobile_native/android/test_anr_android.py
+++ tests/mobile_native/android/test_anr_android.py
@@ -5,15 +5,6 @@ def test_anr_android(android_emu_driver):
     btn = android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[4]')
     btn.click()
 
+    # TODO get the event to send to Sentry.io
     time.sleep(10)
-
     android_emu_driver.launch_app()
-
-'''
-py.test -s -n 4 mobile_native/android/test_unhandlederror_android.py
-py.test -s -n 4 mobile_native/android/test_unhandlederror2_android.py
-py.test -s -n 4 mobile_native/android/test_handlederror_android.py
-py.test -s -n 4 mobile_native/android/test_anr_android.py
-py.test -s -n 4 mobile_native/android/test_nativecrash_android.py
-py.test -s -n 4 mobile_native/android/test_nativemessage_android.py
-'''
--- tests/mobile_native/android/test_nativecrash_android.py
+++ tests/mobile_native/android/test_nativecrash_android.py
@@ -5,6 +5,6 @@ def test_nativecrash_android(android_emu_driver):
     btn = android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[4]')
     btn.click()
 
+    # TODO get the event to send to Sentry.io
     time.sleep(5)
-    
     android_emu_driver.launch_app()
