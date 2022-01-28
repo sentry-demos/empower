173d5c50cfa9c04e3985410d315102ae37161206
William Capozzoli <thinkocapo@gmail.com>
2022-01-27 18:20:22 -0800

    testing

--- tests/mobile_native/android/test_nativecrash_android.py
+++ tests/mobile_native/android/test_nativecrash_android.py
@@ -1,4 +1,10 @@
+import time
+
 # NDK/C++ Native Crash SIGSEGV button
 def test_nativecrash_android(android_emu_driver):
     btn = android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[4]')
     btn.click()
+
+    time.sleep(5)
+    
+    android_emu_driver.launch_app()
--- tests/mobile_native/android/test_unhandlederror_android.py
+++ tests/mobile_native/android/test_unhandlederror_android.py
@@ -2,3 +2,5 @@
 def test_unhandlederror_android(android_emu_driver):
     btn = android_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[1]')
     btn.click()
+    
+    android_emu_driver.launch_app()
