b97ea1d333243c5e84a13f98b7a053eb42e6e6cb
William Capozzoli <thinkocapo@gmail.com>
2022-02-22 15:28:49 -0800

    api servers api/organization

--- ruby/config/routes.rb
+++ ruby/config/routes.rb
@@ -4,5 +4,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   
-  get  'api/organization'
+  
+  # resources :api
+
+  # scope '/api' do
+  #   resources :api
+  # end
+
+  # get 'api/organization'
+  # get 'organization', to: 'api/organization'
+  # get 'organization'
+  # get 'api'
+  get 'api', to: 'api#organization'
 end
