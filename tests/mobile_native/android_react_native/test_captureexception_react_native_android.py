fdf8b1d96ed62f5a0d2d807a08f13355b6ab3339
Chris Stavitsky <corridorr@gmail.com>
2022-05-12 09:56:15 -0700

    Update android react native tests after homescreen/products-screen swap

--- tests/mobile_native/android_react_native/test_captureexception_react_native_android.py
+++ tests/mobile_native/android_react_native/test_captureexception_react_native_android.py
@@ -1,5 +1,9 @@
 import random
 
 def test_captureexception_react_native_android(android_react_native_emu_driver):
+    # click into list app screen
+    android_react_native_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.Button[1]/android.widget.TextView').click()
+    
+    # trigger exception
     btn = android_react_native_emu_driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[5]/android.widget.TextView')
     btn.click()
