794f7320c71e30d1d0053bc4d83e210ecc52fa30
Prithvirajkumar Rajakumar <prithvirajkumar14@gmail.com>
2022-08-04 10:52:11 -0400

    added se and customerType as tags and email address as part of setUser

--- vue/run.sh
+++ vue/run.sh
@@ -9,14 +9,14 @@ RELEASE=$PACKAGE@$VERSION
 echo $RELEASE
 
 SENTRY_ORG=testorg-az
-SENTRY_PROJECT=prithvi-test-vue
+SENTRY_PROJECT=vue
 PREFIX=assets
 
 rm -rf dist
 npm run build
 
 sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $RELEASE
-sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $RELEASE
+sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $RELEASE --ignore-missing
 sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT files $RELEASE upload-sourcemaps --url-prefix "~/assets" --validate dist/$PREFIX
 
 serve -s dist
