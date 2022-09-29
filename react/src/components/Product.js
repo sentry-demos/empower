c3072763bf746bf77cc78953e06039a3dbbdd386
William Capozzoli <thinkocapo@gmail.com>
2022-09-29 10:40:57 +0200

    take first 4 products from response array

--- react/src/components/Product.js
+++ react/src/components/Product.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './product.css';
 import * as Sentry from '@sentry/react';
 import productOne from './products/1';
