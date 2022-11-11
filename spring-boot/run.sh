4805622c47eb89796803ecffac1711e5161b951c
Kosty Maleyev <kosty.maleyev@sentry.io>
2022-11-10 21:42:34 -0800

    [deploy.sh + spring-boot] Make spring-boot work with deploy.sh

--- spring-boot/run.sh
+++ spring-boot/run.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+set -e
+
+properties="./src/main/resources/application.properties"
+
+echo "spring.datasource.url=jdbc:postgresql://$HOST:5432/$DATABASE" >> $properties
+echo "server.port=8090" >> $properties
+echo "spring.cloud.gcp.sql.enabled=false" >> $properties
+
+
 ./mvnw spring-boot:run
