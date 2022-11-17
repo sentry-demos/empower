e06e6dae04d6361a0e602ec6db8d60d88cfbf227
Gideon Lapshun <gideon.lapshun@sentry.io>
2022-11-17 10:43:15 -0500

    my message. fixes APM-DEMO-REACT-A

--- deploy.sh
+++ deploy.sh
@@ -14,13 +14,14 @@ rm -rf build
 npm install
 npm run build
 
-sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $RELEASE
-sentry-cli releases -o $SENTRY_ORG finalize -p $SENTRY_PROJECT $RELEASE
-sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $RELEASE
-sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT files $RELEASE upload-sourcemaps --url-prefix "~/static/js" --validate build/$PREFIX
-sentry-cli deploys -o $SENTRY_ORG new -p $SENTRY_PROJECT -r $RELEASE -e $ENVIRONMENT -n $ENVIRONMENT
+sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $RELEASE --auth-token=$BEARER_TOKEN
+sentry-cli releases -o $SENTRY_ORG finalize -p $SENTRY_PROJECT $RELEASE --auth-token=$BEARER_TOKEN
+# sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $RELEASE --auth-token=$BEARER_TOKEN
+sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT files $RELEASE upload-sourcemaps --url-prefix "~/static/js" --auth-token=$BEARER_TOKEN
+sentry-cli deploys -o $SENTRY_ORG new -p $SENTRY_PROJECT -r $RELEASE -e $ENVIRONMENT -n $ENVIRONMENT --auth-token=$BEARER_TOKEN
 
 # This deploys React - The release was set in the static prod build
-gcloud app deploy --quiet
+# gcloud app deploy --quiet
 
-cd ../flask && gcloud app deploy --quiet
+# cd ../flask && gcloud app deploy --quiet
+npm start
