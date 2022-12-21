cba5886597209b8adfcb9a265ea39a2ae7ba1217
Kosty Maleyev <kosty.maleyev@sentry.io>
2022-12-20 16:16:09 -0800

    Require node 16/npm 8, don't update package-lock every build

--- react/build.sh
+++ react/build.sh
@@ -5,6 +5,9 @@
 set -e # exit immediately if any command exits with a non-zero status
 
 rm -rf build
-npm install
+# npm ci does not update minor versions ->
+# (1) less chance of breaking (2) less noise in PR from package-lock.json
+npm ci 
+# below avoids source map processing error in Sentry when stacktrace includes inline JS
 export INLINE_RUNTIME_CHUNK=false
 npm run build # defined in 'scripts' in package.json
