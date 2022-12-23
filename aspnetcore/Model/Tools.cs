160843f7ef14f47f334521f6c129f6a56e245924
Kosty Maleyev <kosty.maleyev@sentry.io>
2022-12-22 21:23:28 -0800

    [aspnetcore] v0.1 deployable skeleton with DB connection (#133)
    * [aspnetcore] v0.1 deployable skeleton with DB connection

---
+++ aspnetcore/Model/Tools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace aspnetcore.Model
+{
+    public partial class Tools
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Sku { get; set; }
+        public string Image { get; set; }
+        public int Price { get; set; }
+    }
+}
