88fde61ade4dd4f786906f5cd1ac4c358b521243
Angelodaniel <angelo_de_voer@hotmail.com>
2023-01-02 20:58:56 +0100

    added controlers to handle requests

---
+++ aspnetcore/Controllers/OrganizationController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using aspnetcore.Model;
+using Microsoft.Extensions.Configuration;
+
+namespace aspnetcore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrganizationController : ControllerBase
+    {
+        private readonly ILogger<OrganizationController> _logger;
+        private hardwarestoreContext _context = null;
+        private readonly IConfiguration Configuration;
+
+        /* These arguments are wired up automagically by the framework */
+        public OrganizationController(ILogger<OrganizationController> logger, hardwarestoreContext context, IConfiguration configuration)
+        {
+            _logger = logger;
+            _context = context;
+            Configuration = configuration;
+        }
+
+        // seems like this can return any object - will be automatically serialized to JSON
+        [HttpGet]
+        public ActionResult Get()
+        {
+            string dsn = Configuration["SentryDSN"];
+            return Ok("aspnetcore /organization");
+        }
+    }
+}
---
+++ aspnetcore/Controllers/ReviewsController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using aspnetcore.Model;
+using Microsoft.Extensions.Configuration;
+
+namespace aspnetcore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ILogger<ReviewsController> _logger;
+        private hardwarestoreContext _context = null;
+        private readonly IConfiguration Configuration;
+
+        /* These arguments are wired up automagically by the framework */
+        public ReviewsController(ILogger<ReviewsController> logger, hardwarestoreContext context, IConfiguration configuration)
+        {
+            _logger = logger;
+            _context = context;
+            Configuration = configuration;
+        }
+
+        // seems like this can return any object - will be automatically serialized to JSON
+        [HttpGet]
+        public ActionResult Get()
+        {
+            string dsn = Configuration["SentryDSN"];
+            return Ok(_context.Reviews.ToList());
+        }
+    }
+}
