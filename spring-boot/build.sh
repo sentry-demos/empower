9dd8a6fc0252bdb7332b799ba0a8da2b8cf8110e
Kosty Maleyev <kosty.maleyev@sentry.io>
2023-01-07 15:18:42 -0800

    Rename HOST,USERNAME,PASSWORD... -> DB_HOST,DB_USERNAME,DB_PASSWORD,...

--- spring-boot/build.sh
+++ spring-boot/build.sh
@@ -8,11 +8,11 @@ cp ./src/main/resources/application.properties.template $properties
 
 echo "" >> $properties # newline
 echo "sentry.dsn=$SPRINGBOOT_APP_DSN" >> $properties
-echo "spring.datasource.username=$USERNAME" >> $properties
-echo "spring.datasource.password=$PASSWORD" >> $properties
-echo "spring.cloud.gcp.sql.database-name=$DATABASE" >> $properties
+echo "spring.datasource.username=$DB_USERNAME" >> $properties
+echo "spring.datasource.password=$DB_PASSWORD" >> $properties
+echo "spring.cloud.gcp.sql.database-name=$DB_DATABASE" >> $properties
 #This value is formatted in the form: [gcp-project-id]:[region]:[instance-name]
-echo "spring.cloud.gcp.sql.instance-connection-name=$CLOUD_SQL_CONNECTION_NAME" >> $properties
+echo "spring.cloud.gcp.sql.instance-connection-name=$DB_CLOUD_SQL_CONNECTION_NAME" >> $properties
 
 #GCP
 # NOTE: if run.sh is called after build.sh it must overwrite this value
