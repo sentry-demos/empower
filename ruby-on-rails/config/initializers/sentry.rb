92a5a71ca9f177875df15789a2d62eede747feed
Neil Manvar <neil@sentry.io>
2023-01-13 14:30:07 -0800

    read from env var (dsn + release)

--- ruby-on-rails/config/initializers/sentry.rb
+++ ruby-on-rails/config/initializers/sentry.rb
@@ -1,10 +1,9 @@
 Sentry.init do |config|
-  config.dsn = 'https://56904659106448b68eb7a2207babe1e6@o87286.ingest.sentry.io/4504456042774528'
+  config.dsn = ENV['RUBYONRAILS_APP_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  # TODO: set release which will enable sessions
-  # config.release = 'abc'
-  config.environment = ENV['RAILS_ENV']
+  config.release = ENV['RELEASE']
+  config.environment = ENV['RAILS_ENV'] || "development"
 
   config.traces_sample_rate = 1.0
 end
