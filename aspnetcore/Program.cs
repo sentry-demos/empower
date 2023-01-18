e9bc89fb79c6b712c55f08a108d5dba412fa1a2a
Angelodaniel <angelo_de_voer@hotmail.com>
2023-01-18 14:13:54 +0100

    dsn as a variable instead of fixed

--- aspnetcore/Program.cs
+++ aspnetcore/Program.cs
@@ -21,14 +21,9 @@ namespace aspnetcore
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-
                     webBuilder.UseSentry(o => {
-                        o.Dsn = "https://f3f027e29a4b4a15aadbefad976b293b@o87286.ingest.sentry.io/4504412169699329";
-                        // When configuring for the first time, to see what the SDK is doing:
-                        o.Debug = true;
-                        // Set TracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-                        // We recommend adjusting this value in production.
-                        o.TracesSampleRate = 1.0;
+                        o.Dsn = Environment.GetEnvironmentVariable("ASPNETCORE_APP_DSN");
+                        o.TracesSampleRate = 1.0; 
                     });
                 });
     }
