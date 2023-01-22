563c1f29989fef16c55e9f32d62ce8809affc7a3
Kosty Maleyev <kosty.maleyev@sentry.io>
2023-01-22 10:41:58 -0800

    Fix CORS in Laravel by cleaning up dead code, legacy scaffolding

--- laravel/app/Http/Middleware/TrustProxies.php
+++ laravel/app/Http/Middleware/TrustProxies.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Middleware;
-use Fideloper\Proxy\TrustProxies as Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
