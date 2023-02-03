6159b9e6805c2c277f8867ee7cd02d5a8fef5831
Kosty Maleyev <kosty.maleyev@sentry.io>
2023-02-03 15:59:26 -0800

    [tda] Upgrade Selenium -> 4, Appium-Python-Driver, other dependencies, remove unused
    fix for upgrade
    
    [tda] Update mobile_native to use latest Appium-Python-Client 2.x (still old Appium 1.x server)
    
    fix apium upgr
    
    fix upgr
    
    fix upgr

--- tests/mobile_native/android/test_handlederror_android.py
+++ tests/mobile_native/android/test_handlederror_android.py
@@ -1,4 +1,5 @@
 import sentry_sdk
+from appium.webdriver.common.appiumby import AppiumBy
 
 # Clicks the Handled Error button that says ArrayIndexOutOfBoundsException
 def test_handlederror_android(android_emu_driver):
@@ -6,11 +7,11 @@ def test_handlederror_android(android_emu_driver):
 
     try:
         # navigate to list app
-        android_emu_driver.find_element_by_accessibility_id('More').click()
-        android_emu_driver.find_element_by_id('com.example.vu.android:id/content').click()
+        android_emu_driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'More').click()
+        android_emu_driver.find_element(AppiumBy.ID, 'com.example.vu.android:id/content').click()
 
         # trigger error
-        android_emu_driver.find_element_by_id('com.example.vu.android:id/handled_exception').click()
+        android_emu_driver.find_element(AppiumBy.ID, 'com.example.vu.android:id/handled_exception').click()
         
     except Exception as err:
         sentry_sdk.capture_exception(err)
