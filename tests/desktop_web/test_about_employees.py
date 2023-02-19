794f836fb7362f716d829fff64a1f915672ad771
Kosty Maleyev <kosty.maleyev@sentry.io>
2023-02-19 10:08:02 -0800

    [tda] refactor: endpoints and selenium_endpoint fixtures

--- tests/desktop_web/test_about_employees.py
+++ tests/desktop_web/test_about_employees.py
@@ -1,18 +1,13 @@
 import time
-import yaml
 import random
 import sentry_sdk
 import pytest
 from urllib.parse import urlencode
 
-def test_about_employees(desktop_web_driver):
+def test_about_employees(desktop_web_driver, endpoints):
     sentry_sdk.set_tag("pytestName", "test_about_employees")
 
-    with open('endpoints.yaml', 'r') as stream:
-        data_loaded = yaml.safe_load(stream)
-        endpoints = data_loaded['react_endpoints']
-
-    for endpoint in endpoints:
+    for endpoint in endpoints['react_endpoints']:
         endpoint_about = endpoint + "/about"
         sentry_sdk.set_tag("endpoint", endpoint_about)
 
