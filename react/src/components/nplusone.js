2e8d7b2d50be6125d033374a714031f04156f55a
Ben Pepper <benpepper@RN9TWMY3WC.local>
2023-02-22 09:16:41 -0800

    It works.

--- react/src/components/nplusone.js
+++ react/src/components/nplusone.js
@@ -1,42 +1,53 @@
-import { Component } from 'react';
-import Context from '../utils/context';
-import { Link, useFetcher } from 'react-router-dom';
-import * as Sentry from '@sentry/react';
-import { connect } from 'react-redux';
-import { determineBackendUrl } from '../utils/backendrouter';
-
-function componentDidMount() {
-  console.log(this.props);
-
-  let urls = async () => {
-    let i=0;
-    let urllist=[]
-    for(i;i< this.state.data.length;i++){
-        const response = await fetch(this.props.backend + '/product/0/info?id=' + i)
-        console.log(this);
-        console.log(this.props.backend);
-        console.log(this.props.backend + '/product/0/info?id=' + i);
-        const json = await response.json()
-        urllist.push(json.items[0])
-        console.log({urllist})
-      }
-   }
-  
-  // fetch(for (let i = 0; i < 10; i++) {
-  //   let response = fetch Promise.all(this.props.backend + '/product/0/info?id=' + i);
-  // }) 
+import { Component } from "react";
+import Context from "../utils/context";
+import { Link, useFetcher } from "react-router-dom";
+import * as Sentry from "@sentry/react";
+import { connect } from "react-redux";
+import { determineBackendUrl } from "../utils/backendrouter";
+class NPlusOneItem extends Component {
+  async componentDidMount() {
+    console.log("Mounting NPlusOneItem");
+    const response = await fetch(this.props.backend + "/product/0/info?id=" + this.props.index);
+    console.log({ response });
+    const json = await response.json();
+    console.log({ json });
+    this.setState({
+      data: json,
+    });
+  }
+  render() {
+    return <li>{this.props.index}</li>;
+  }
 }
-
 class Nplusone extends Component {
-  
-    render() {
-        return (
-          <div>
-            <h1>API N+1 Performance Issue</h1>
-              <h3>look at the query <a href="https://testorg-az.sentry.io/issues/?project=5808623&query=is%3Aunresolved+issue.type%3Aperformance_n_plus_one_api_calls&referrer=issue-list&statsPeriod=14d" rel="noreferrer">here</a></h3>
-          </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    const ids = new Array(20).fill(0).map((item, index) => index);
+    this.state = {
+      ids,
+    };
+  }
+  async componentDidMount() {
+    console.log("Mounting NPlusOne with props", this.props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>API N+1 Performance Issue</h1>
+        {this.state.ids.map((index) => (
+          <NPlusOneItem key={index} index={index} backend={this.props.backend} />
+        ))}
+        <h3>
+          look at the query{" "}
+          <a
+            href="https://testorg-az.sentry.io/issues/?project=5808623&query=is%3Aunresolved+issue.type%3Aperformance_n_plus_one_api_calls&referrer=issue-list&statsPeriod=14d"
+            rel="noreferrer"
+          >
+            here
+          </a>
+        </h3>
+      </div>
+    );
+  }
 }
-
 export default Nplusone;
