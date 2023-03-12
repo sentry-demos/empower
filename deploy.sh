767f43fff47b806d282cf9d8b396e1b78371bff0
Kosty Maleyev <kosty.maleyev@sentry.io>
2023-03-11 16:40:09 -0800

    fix changes accidentally committed in "Adds N+1 Functionality  (#177)"

--- deploy.sh
+++ deploy.sh
@@ -165,7 +165,7 @@ for proj in $projects; do # bash only
   ./build.sh
 
   if [[ "$fe_projects" = *"$proj "* ]]; then # project is frontend
-    bash -x sentry-release.sh $env $RELEASE
+    sentry-release.sh $env $RELEASE
     # NOTE: Sentry may create releases from events even without this step
   fi
 
