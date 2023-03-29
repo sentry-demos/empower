69756366d12015d6a8cd42125fd1633a23bdee49
Neil Manvar <neil@sentry.io>
2023-03-29 08:31:19 -0700

    use backend query_string in test_organization.py

--- tests/desktop_web/test_organization.py
+++ tests/desktop_web/test_organization.py
@@ -1,18 +1,23 @@
 import time
 import pytest
 import sentry_sdk
+from urllib.parse import urlencode
 
-def test_organization(desktop_web_driver, endpoints, random, batch_size, sleep_length):
+def test_organization(desktop_web_driver, endpoints, random, batch_size, backend, sleep_length):
     sentry_sdk.set_tag("pytestName", "test_organization")
 
     for endpoint in endpoints['react_endpoints']:
         endpoint_organization = endpoint + "/organization"
         sentry_sdk.set_tag("endpoint", endpoint_organization)
 
-        url = endpoint_organization
+        query_string = {
+            # only run against 'flask'
+            'backend': backend(exclude=['express','springboot','ruby','laravel'])
+        }
+        url = endpoint_organization + '?' + urlencode(query_string)
 
         for i in range(batch_size):
-        
+
             desktop_web_driver.get(url)
 
             # images are being loaded in /about from Cloud Storage
