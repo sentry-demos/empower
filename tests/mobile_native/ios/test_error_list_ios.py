19b12c59fa986a59708410101643ad4abd2f133b
Neil Manvar <neil@sentry.io>
2023-04-05 08:49:14 -0700

    add test for errors list, hits all buttons that cause error events (#199)
    * add test for errors list, hits all buttons that cause error events
    
    * remove sleep comments

---
+++ tests/mobile_native/ios/test_error_list_ios.py
@@ -0,0 +1,34 @@
+import sentry_sdk
+import time
+from appium.webdriver.common.appiumby import AppiumBy
+
+def test_errorlist_ios(ios_sim_driver):
+
+    try:
+        ios_sim_driver.find_element(AppiumBy.ACCESSIBILITY_ID, "more").click()
+        ios_sim_driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="Error"]').click()
+        ios_sim_driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="NSException"]').click()
+        ios_sim_driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="Fatal Error"]').click()
+        ios_sim_driver.launch_app()
+
+        ios_sim_driver.find_element(AppiumBy.ACCESSIBILITY_ID, "more").click()
+        ios_sim_driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="DiskWriteException (!)"]').click()
+        ios_sim_driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="HighCPULoad"]').click()
+        ios_sim_driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="Permissions (!)"]').click()
+        ios_sim_driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="Async Crash (!)"]').click()
+        ios_sim_driver.launch_app()
+
+        ios_sim_driver.find_element(AppiumBy.ACCESSIBILITY_ID, "more").click()
+        ios_sim_driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="ANR Fully Blocking"]').click()
+        ios_sim_driver.launch_app()
+
+        ios_sim_driver.find_element(AppiumBy.ACCESSIBILITY_ID, "more").click()
+        ios_sim_driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="ANR Filling Run Loop"]').click()
+
+        ios_sim_driver.launch_app()
+
+        # wait for confirmation of purchase? (currently nothing happens)
+
+    except Exception as err:
+        # sentry_sdk.capture_exception(err)
+        raise err
