ecc9755416670dacc02234a1f1f1512ff1f18598
Angelodaniel <angelo_de_voer@hotmail.com>
2023-04-26 11:16:59 +0200

    [dotnet] - Updated useEndpoints for transactions

--- aspnetcore/Startup.cs
+++ aspnetcore/Startup.cs
@@ -68,9 +68,7 @@ namespace aspnetcore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
