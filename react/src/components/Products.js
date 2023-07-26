ced9c016400a9f8ee6b1eee2c1ebd0414403c27b
JonasBa <jonas@badalic.com>
2023-07-26 09:59:28 -0400

    feat(profiling): introduce a large list render

--- react/src/components/Products.js
+++ react/src/components/Products.js
@@ -2,11 +2,11 @@ import { Component } from 'react';
 import Context from '../utils/context';
 import './products.css';
 import * as Sentry from '@sentry/react';
-import { connect } from 'react-redux'
-import { setProducts, addProduct } from '../actions'
-import Loader from "react-loader-spinner";
-import { busy_sleep } from "../utils/time"
-import ProductCard from './ProductCard'
+import { connect } from 'react-redux';
+import { setProducts, addProduct } from '../actions';
+import Loader from 'react-loader-spinner';
+import { busy_sleep } from '../utils/time';
+import ProductCard from './ProductCard';
 
 class Products extends Component {
   static contextType = Context;
@@ -19,53 +19,59 @@ class Products extends Component {
 
   // getProducts handles error responses differently, depending on the browser used
   async getProducts() {
-    let se, customerType, email
-    Sentry.withScope(function(scope) {
-      [ se, customerType ] = [scope._tags.se, scope._tags.customerType ]
-      email = scope._user.email
+    let se, customerType, email;
+    Sentry.withScope(function (scope) {
+      [se, customerType] = [scope._tags.se, scope._tags.customerType];
+      email = scope._user.email;
     });
 
     ['/api', '/connect', '/organization'].forEach((endpoint) => {
       fetch(this.props.backend + endpoint, {
-        method: "GET",
-        headers: { se, customerType, email, "Content-Type": "application/json" }
-      })
-        .catch((err) => { 
-          // If there's an error, it won't stop the Products http request and page from loading
-          Sentry.captureException(err)
-        })
-    })
+        method: 'GET',
+        headers: {
+          se,
+          customerType,
+          email,
+          'Content-Type': 'application/json',
+        },
+      }).catch((err) => {
+        // If there's an error, it won't stop the Products http request and page from loading
+        Sentry.captureException(err);
+      });
+    });
 
-    let result = await fetch(this.props.backend + "/products", {
-      method: "GET",
-      headers: { se, customerType, email, "Content-Type": "application/json" }
-    })
-      .catch((err) => { 
-        return { ok: false, status: 500}
-      })
+    let result = await fetch(this.props.backend + '/products', {
+      method: 'GET',
+      headers: { se, customerType, email, 'Content-Type': 'application/json' },
+    }).catch((err) => {
+      return { ok: false, status: 500 };
+    });
 
     if (!result.ok) {
-      Sentry.configureScope(function(scope) {
-        Sentry.setContext("err", {
+      Sentry.configureScope(function (scope) {
+        Sentry.setContext('err', {
           status: result.status,
-          statusText: result.statusText
-        })
+          statusText: result.statusText,
+        });
       });
       return Promise.reject();
     } else {
-      const jsonValue = await result.json()
-      return Promise.resolve(jsonValue)
+      const jsonValue = await result.json();
+      return Promise.resolve(jsonValue);
     }
   }
 
-  async componentDidMount(){
-    var products
+  async componentDidMount() {
+    var products;
     try {
       products = await this.getProducts();
       // take first 4 products because that's all we have img/title/description for
-      this.props.setProducts(products.slice(0,4))
-    } catch(err) {
-      Sentry.captureException(new Error("app unable to load products: " + err));
+      this.props.setProducts(Array(250/4).fill(products.slice(0, 4)).flat().map((p, n) => {
+        p.id = n
+        return p
+      }));
+    } catch (err) {
+      Sentry.captureException(new Error('app unable to load products: ' + err));
     }
   }
 
@@ -74,44 +80,53 @@ class Products extends Component {
     return products.length > 0 ? (
       <div>
         <ul className="products-list">
-          {products.map((product) => {
-            const averageRating = (product.reviews.reduce((a,b) => a + (b["rating"] || 0),0) / product.reviews.length).toFixed(1)
+          {products.map((product,i) => {
+            const averageRating = (
+              product.reviews.reduce((a, b) => a + (b['rating'] || 0), 0) /
+              product.reviews.length
+            ).toFixed(1);
 
-            let stars = [1,2,3,4,5].map((index) => {
+            let stars = [1, 2, 3, 4, 5].map((index) => {
               if (index <= averageRating) {
-                return (<span className="star" key={index}>&#9733;</span>)
+                return (
+                  <span className="star" key={index}>
+                    &#9733;
+                  </span>
+                );
               } else {
-                return (<span className="star" key={index}>&#9734;</span>)
+                return (
+                  <span className="star" key={index}>
+                    &#9734;
+                  </span>
+                );
               }
-            })
+            });
 
             return (
-              <ProductCard key={product.id} product={product} stars={stars}></ProductCard>
+              <ProductCard
+                key={i}
+                product={product}
+                stars={stars}
+              ></ProductCard>
             );
           })}
         </ul>
       </div>
     ) : (
       <div className="loader-container">
-        <Loader
-          type="ThreeDots"
-          color="#f6cfb2"
-          height={150}
-          width={150}
-          />
+        <Loader type="ThreeDots" color="#f6cfb2" height={150} width={150} />
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
   return {
     cart: state.cart,
-    products: state.products
-  }
-}
+    products: state.products,
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  { setProducts, addProduct }
-)(Sentry.withProfiler(Products, { name: "Products"}))
+export default connect(mapStateToProps, { setProducts, addProduct })(
+  Sentry.withProfiler(Products, { name: 'Products' })
+);
