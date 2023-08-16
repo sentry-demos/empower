4b9a5bcf1b3919d5707ab5f06b1e7d0531398796
Chris Stavitsky <12092849+cstavitsky@users.noreply.github.com>
2023-08-16 14:09:31 -0700

    format remaining files in react/ dir

--- react/config-overrides.js
+++ react/config-overrides.js
@@ -2,14 +2,16 @@ const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
-  config.plugins.push(SentryWebpackPlugin.sentryWebpackPlugin({
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-    include: '.',
-    org: 'testorg-az',
-    project: "frontend-javascript",
-    ignoreFile: '.sentrycliignore',
-    ignore: ['webpack.config.js'],
-    configFile: 'sentry.properties',
-  }))
+  config.plugins.push(
+    SentryWebpackPlugin.sentryWebpackPlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      include: '.',
+      org: 'testorg-az',
+      project: 'frontend-javascript',
+      ignoreFile: '.sentrycliignore',
+      ignore: ['webpack.config.js'],
+      configFile: 'sentry.properties',
+    })
+  );
   return config;
-}
+};
