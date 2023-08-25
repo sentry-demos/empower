af868fe2985a1d997652aed2d4422b9a7829bdf0
Chris Stavitsky <12092849+cstavitsky@users.noreply.github.com>
2023-08-24 17:09:22 -0700

    auto format express dir

--- express/server.js
+++ express/server.js
@@ -12,43 +12,43 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-'use strict';
-const axios = require('axios');
+"use strict";
+const axios = require("axios");
 
 // Imported Functions
-const DB = require('./db');
+const DB = require("./db");
 
 // Utils
-const utils = require('./utils');
+const utils = require("./utils");
 
 // [START app]
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-var headers = {}
-const sentryEventContext = function(req, res, next) {
+const cors = require("cors");
+var headers = {};
+const sentryEventContext = function (req, res, next) {
   const se = req.headers.se;
 
-  if(se !== undefined) {
+  if (se !== undefined) {
     Sentry.setTag("se", se);
-    headers["se"]=se
+    headers["se"] = se;
   }
 
   const customerType = req.headers.customertype;
-  if(customerType !== undefined) {
+  if (customerType !== undefined) {
     Sentry.setTag("customerType", customerType);
-    headers["customerType"]=customerType
+    headers["customerType"] = customerType;
   }
 
   const email = req.headers.email;
-  if(email !== undefined) {
-    Sentry.setUser({ 'email': email })
-    headers["email"]=email
+  if (email !== undefined) {
+    Sentry.setUser({ email: email });
+    headers["email"] = email;
   }
 
   // keep executing the router middleware
   next();
-}
+};
 
 const dsn = process.env.EXPRESS_APP_DSN;
 const release = process.env.RELEASE;
@@ -60,9 +60,9 @@ console.log("> RELEASE", release);
 console.log("> ENVIRONMENT", environment);
 
 // Initialize Sentry
-const Sentry = require('@sentry/node');
-const Tracing = require('@sentry/tracing');
-const { ProfilingIntegration } = require('@sentry/profiling-node');
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+const { ProfilingIntegration } = require("@sentry/profiling-node");
 Sentry.init({
   dsn: dsn,
   environment: environment,
@@ -77,49 +77,50 @@ Sentry.init({
   ],
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
-  tracesSampler: samplingContext => {
+  tracesSampler: (samplingContext) => {
     // sample out transactions from http OPTIONS requests hitting endpoints
-    const request = samplingContext.request
-    if (request && request.method == 'OPTIONS') {
-      return 0.0
-    }  else {
-      return 1.0
+    const request = samplingContext.request;
+    if (request && request.method == "OPTIONS") {
+      return 0.0;
+    } else {
+      return 1.0;
     }
   },
-  includeLocalVariables: true
-})
+  includeLocalVariables: true,
+});
 
-async function fetchProducts(req, res){
+async function fetchProducts(req, res) {
   try {
     // This /api call must happen before the DB.products() call or else it's a broken subtrace (if you do it after DB.Products())
-    await axios.get(RUBY_BACKEND + "/api", {headers: headers} )
-      .then(response => {
-        console.log("> response", response.data)
-        return
+    await axios
+      .get(RUBY_BACKEND + "/api", { headers: headers })
+      .then((response) => {
+        console.log("> response", response.data);
+        return;
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-        Sentry.captureException(error)
+        Sentry.captureException(error);
       });
 
-    let transaction = Sentry.getCurrentHub()
-      .getScope()
-      .getTransaction();
+    let transaction = Sentry.getCurrentHub().getScope().getTransaction();
 
     const products = await DB.getProducts();
 
-    let profilingSpan = transaction.startChild({ op: '/products.get_iterator', description: 'function'});
+    let profilingSpan = transaction.startChild({
+      op: "/products.get_iterator",
+      description: "function",
+    });
 
     await utils.getIteratorProcessor(products);
-    
+
     profilingSpan.finish();
     transaction.finish();
 
     res.status(200).send(products);
-
   } catch (error) {
     Sentry.captureException(error);
-    throw(error);
+    throw error;
   }
 }
 
@@ -130,60 +131,62 @@ app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 
 app.use(cors());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(sentryEventContext);
 
 // Configure ENV
-require('dotenv').config();
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-  res.send('Sentry Express Service says Hello - turn me into a microservice that powers Payments, Shipping, or Customers');
+app.get("/", (req, res) => {
+  res.send(
+    "Sentry Express Service says Hello - turn me into a microservice that powers Payments, Shipping, or Customers"
+  );
 });
 
-app.get('/success', (req, res) => {
-  console.log("> success")
+app.get("/success", (req, res) => {
+  console.log("> success");
   res.send(`success from express`);
 });
 
-app.get('/products', fetchProducts);
+app.get("/products", fetchProducts);
 
-app.get('/products-join', async(req, res) => {
+app.get("/products-join", async (req, res) => {
   try {
     // This /api call must happen before the DB.products() call or else it's a broken subtrace (if you do it after DB.Products())
-    await axios.get(RUBY_BACKEND + "/api", {headers: headers} )
-      .then(response => {
-        console.log("> response", response.data)
-        return
+    await axios
+      .get(RUBY_BACKEND + "/api", { headers: headers })
+      .then((response) => {
+        console.log("> response", response.data);
+        return;
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-        Sentry.captureException(error)
+        Sentry.captureException(error);
       });
 
-    let transaction = Sentry.getCurrentHub()
-      .getScope()
-      .getTransaction();
-    let span = transaction.startChild({ op: '/products.get_products_join', description: 'function' });
+    let transaction = Sentry.getCurrentHub().getScope().getTransaction();
+    let span = transaction.startChild({
+      op: "/products.get_products_join",
+      description: "function",
+    });
     const products = await DB.getJoinedProducts();
     span.finish();
 
     res.status(200).send(products);
   } catch (error) {
     Sentry.captureException(error);
-    throw(error);
+    throw error;
   }
 });
 
-app.post('/checkout', async(req, res) => {
+app.post("/checkout", async (req, res) => {
   const order = req.body;
-  const cart = order['cart'];
-  const form = order['form'];
+  const cart = order["cart"];
+  const form = order["form"];
   let inventory = [];
   try {
-    const transaction = Sentry.getCurrentHub()
-      .getScope()
-      .getTransaction();
+    const transaction = Sentry.getCurrentHub().getScope().getTransaction();
 
     // Get Inventory
     let spanGetInventory = transaction.startChild({
@@ -200,35 +203,34 @@ app.post('/checkout', async(req, res) => {
       op: "function",
       description: "processOrder",
     });
-    let quantities = cart['quantities'];
+    let quantities = cart["quantities"];
     console.log("quantities", quantities);
-    for(const cartItem in quantities) {
-      for(const inventoryItem of inventory) {
-        console.log("> inventoryItem.count", inventoryItem['count']);
-        if(inventoryItem.count < quantities[cartItem]) {
+    for (const cartItem in quantities) {
+      for (const inventoryItem of inventory) {
+        console.log("> inventoryItem.count", inventoryItem["count"]);
+        if (inventoryItem.count < quantities[cartItem]) {
           throw new Error("Not enough inventory for product");
         }
       }
     }
     spanProcessOrder.finish();
 
-    res.status(200).send('success');
+    res.status(200).send("success");
   } catch (error) {
     Sentry.captureException(error);
     res.status(500).send(error);
   }
-
 });
 
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   res.send(`express /api`);
 });
 
-app.get('/connect', (req, res) => {
+app.get("/connect", (req, res) => {
   res.send(`express /connect`);
 });
 
-app.get('/organization', (req, res) => {
+app.get("/organization", (req, res) => {
   res.send(`express /organization`);
 });
 
