d59d29e4761d934a15f19437364fae168bcebc1a
anthony sottile <anthony.sottile@sentry.io>
2023-09-27 14:47:12 -0400

    react: perform all build-related parts in package.json:build

--- react/build.sh
+++ react/build.sh
@@ -7,9 +7,5 @@ set -e # exit immediately if any command exits with a non-zero status
 rm -rf build
 # npm ci does not update minor versions ->
 # (1) less chance of breaking (2) less noise in PR from package-lock.json
-npm ci 
-# below avoids source map processing error in Sentry when stacktrace includes inline JS
-export INLINE_RUNTIME_CHUNK=false
+npm ci
 npm run build # defined in 'scripts' in package.json
-
-cp ./serve.json build/serve.json
