6716f2f23a6e13856f35361de082fe8013f434a2
Jonas <jonas@badalic.com>
2023-09-27 19:53:23 -0400

    fix(build): add react-prod-sourcemaps (#287)

--- react/config-overrides.js
+++ react/config-overrides.js
@@ -1,7 +1,14 @@
 const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+const reactsourceMapPlugin = require('@acemarke/react-prod-sourcemaps');
 
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
+  config.plugins.push(
+    reactsourceMapPlugin.WebpackReactSourcemapsPlugin({
+      mode: 'strict',
+    })
+  );
+
   config.plugins.push(
     SentryWebpackPlugin.sentryWebpackPlugin({
       authToken: process.env.SENTRY_AUTH_TOKEN,
