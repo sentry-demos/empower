b22b08868ddf2981a824b9a240c72dfa017fcd40
Kosty Maleyev <kosty.maleyev@sentry.io>
2023-10-31 16:28:24 -0700

    [crons-python] automate crontab, change host, rename project & monitor

--- crons-python/main.py
+++ crons-python/main.py
@@ -4,16 +4,16 @@ import random
 import urllib.request
 import time
 
-# cron-job-python
+# crons-python
 DSN = "https://5c0f7b0814074657819c61bee198d316@o87286.ingest.sentry.io/4505197623705600"
-FAILURE_PERCENT_CHANCE = 10
-STUCK_PERCENT_CHANCE = 10
+FAILURE_PERCENT_CHANCE = 5 
+STUCK_PERCENT_CHANCE = 7 
 STUCK_SLEEP_MIN_MINUTES = 6
 STUCK_SLEEP_MAX_MINUTES = 20
 
 random_number = random.randint(0, 99)
 
-@monitor(monitor_slug='cron-job-monitor-python')
+@monitor(monitor_slug='crons-python-monitor')
 def job():
     if random_number < FAILURE_PERCENT_CHANCE:
         print("Attempting to call an API that is down.")
--- crons-python/run.sh
+++ crons-python/run.sh
@@ -4,4 +4,4 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 source env/bin/activate
-python3 main.py
+python3.10 main.py
