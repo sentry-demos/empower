63e2dc2dfedf482c9c1e58193d12c24dd6e09dcd
Kosty Maleyev <kosty.maleyev@sentry.io>
2023-10-31 11:14:03 -0700

    fix deploy_tda.sh by upgrading Ubuntu

--- tda/build.sh
+++ tda/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e 
-virtualenv --python=`which python3.8` env
+#virtualenv --python=`which python3.8` env
+python3.10 -m venv env
 source env/bin/activate 
 pip install -r requirements.txt
