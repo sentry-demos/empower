689e1618ea7cc5026fe29ac439d7a92cc961719c
Abhijeet Prasad <aprasad@sentry.io>
2023-11-06 21:00:56 -0500

    feat(react): Add error boundary to form container (#314)

--- react/src/components/Footer.js
+++ react/src/components/Footer.js
@@ -1,34 +1,61 @@
-import { Component } from 'react';
+import * as Sentry from '@sentry/react';
+import { Fragment, Component } from 'react';
 import { Link } from 'react-router-dom';
 import './footer.css';
 
 class Footer extends Component {
+  state = {
+    subscribed: false,
+  };
+
   async shouldComponentUpdate() {
     console.log('> Footer shouldComponentUpdate');
   }
 
   render() {
+    const handleSubmit = () => {
+      this.setState({ subscribed: true });
+    };
+
     return (
       <footer id="footer">
         <div>
           <h2 className="h3 sentry-unmask">Sign up for plant tech news</h2>
-          <div class="formContainer">
-            <form>
-              <label htmlFor="email-subscribe" className="sentry-unmask">
-                Email
-              </label>
+          <Sentry.ErrorBoundary
+            onReset={() => {
+              this.setState({ subscribed: false });
+            }}
+            fallback={({ resetError, eventId }) => {
+              return (
+                <Fragment>
+                  <div>An error has occurred. Sentry Event ID: {eventId}</div>
+                  <button className="btn" onClick={resetError}>
+                    Reset Form
+                  </button>
+                </Fragment>
+              );
+            }}
+          >
+            <div className="formContainer">
+              <form onSubmitCapture={handleSubmit}>
+                <label htmlFor="email-subscribe" className="sentry-unmask">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email-subscribe"
+                  id="email-subscribe"
+                ></input>
+              </form>
               <input
-                type="email"
-                name="email-subscribe"
-                id="email-subscribe"
-              ></input>
-            </form>
-            <input
-              type="submit"
-              value="Subscribe"
-              className="sentry-unmask"
-            ></input>
-          </div>
+                type="submit"
+                value="Subscribe"
+                className="sentry-unmask"
+                onClick={handleSubmit}
+              />
+              {this.state.subscribed && <SubscribedMessage />}
+            </div>
+          </Sentry.ErrorBoundary>
           <p className="sentry-unmask">
             © 2021 • Empower Plant • <Link to="/about">About us</Link>
           </p>
@@ -36,6 +63,10 @@ class Footer extends Component {
       </footer>
     );
   }
+}
+
+function SubscribedMessage() {
+  throw new Error('SubscribedMessage error');
 }
 
 export default Footer;
