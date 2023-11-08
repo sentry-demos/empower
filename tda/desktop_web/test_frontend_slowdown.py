c442964bd966e30d084335e90cf3466f5306f113
Chris Stavitsky <12092849+cstavitsky@users.noreply.github.com>
2023-11-08 10:27:10 -0800

    TDA test to generate data for frontend slowdown page

---
+++ tda/desktop_web/test_frontend_slowdown.py
@@ -0,0 +1,24 @@
+import time
+import sentry_sdk
+from urllib.parse import urlencode
+
+def test_frontend_slowdown(desktop_web_driver, endpoints, random, batch_size, backend, sleep_length):
+
+    for endpoint in endpoints.react_endpoints:
+        endpoint_products_join = endpoint + "/products-fes"
+        sentry_sdk.set_tag("endpoint", endpoint_products_join)
+
+        for i in range(batch_size):
+            # Ensures a different backend endpoint gets picked each time
+            url = ""
+            # TODO make a query_string builder function for sharing this across tests
+            query_string = {
+                # 'ruby' /products /checkout endpoints not available yet
+                'backend': backend(exclude=['ruby', 'laravel', 'aspnetcore'])
+            }
+            url = endpoint_products_join + '?' + urlencode(query_string)
+
+            desktop_web_driver.get(url)
+            time.sleep(sleep_length() + sleep_length() + 1)
+
+        # Checkout button not clicked yet in frontend slowdown flow
