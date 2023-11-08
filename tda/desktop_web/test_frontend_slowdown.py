22c7a664a4ed161966a932e5af324ffc51a9bcc8
Chris Stavitsky <12092849+cstavitsky@users.noreply.github.com>
2023-11-08 10:41:23 -0800

    improve variable name

--- tda/desktop_web/test_frontend_slowdown.py
+++ tda/desktop_web/test_frontend_slowdown.py
@@ -5,8 +5,8 @@ from urllib.parse import urlencode
 def test_frontend_slowdown(desktop_web_driver, endpoints, random, batch_size, backend, sleep_length):
 
     for endpoint in endpoints.react_endpoints:
-        endpoint_products_join = endpoint + "/products-fes"
-        sentry_sdk.set_tag("endpoint", endpoint_products_join)
+        endpoint_frontend_slowdown = endpoint + "/products-fes"
+        sentry_sdk.set_tag("endpoint", endpoint_frontend_slowdown)
 
         for i in range(batch_size):
             # Ensures a different backend endpoint gets picked each time
@@ -16,7 +16,7 @@ def test_frontend_slowdown(desktop_web_driver, endpoints, random, batch_size, ba
                 # 'ruby' /products /checkout endpoints not available yet
                 'backend': backend(exclude=['ruby', 'laravel', 'aspnetcore'])
             }
-            url = endpoint_products_join + '?' + urlencode(query_string)
+            url = endpoint_frontend_slowdown + '?' + urlencode(query_string)
 
             desktop_web_driver.get(url)
             time.sleep(sleep_length() + sleep_length() + 1)
