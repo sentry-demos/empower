9153a6176e35dd5a829ebb44aba9f97e025a823a
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2023-11-27 09:25:31 -0500

    [mini-relay] produce loud errors on unexpected routes (#334)

--- mini-relay/app.py
+++ mini-relay/app.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import flask
 
 app = flask.Flask(__name__)
@@ -9,3 +11,11 @@ def envelope(p: str) -> flask.Response:
     ret = app.make_response(('', 204))
     ret.access_control_allow_origin = '*'
     return ret
+
+
+@app.post('/<path:path>')
+def unhandled(path: str) -> tuple[str, int]:
+    print('*' * 79)
+    print(f'unhandled path: {path}')
+    print('*' * 79)
+    return '', 400
