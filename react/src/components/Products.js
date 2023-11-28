4922643b4dc950667939e9c5f407ca23f92d449b
Chris Stavitsky <12092849+cstavitsky@users.noreply.github.com>
2023-11-28 12:28:54 -0800

    [React] Make browser profile frame human readable (#350)
    * WIP - refactor to remove async method that messes up readability of profile frame
    
    * create named function that displays in profile frame
    
    * add comment for reasoning behind change

--- react/src/components/Products.js
+++ react/src/components/Products.js
@@ -1,96 +1,56 @@
 import { Component } from 'react';
-import Context from '../utils/context';
 import './products.css';
 import * as Sentry from '@sentry/react';
 import { connect } from 'react-redux';
 import { setProducts, addProduct } from '../actions';
 import Loader from 'react-loader-spinner';
-import { busy_sleep } from '../utils/time';
 import ProductCard from './ProductCard';
+import { useState, useEffect } from 'react';
 
-class Products extends Component {
-  static contextType = Context;
+function Products({ frontendSlowdown, backend }) {
+  const [products, setProducts] = useState([]);
 
-  constructor() {
-    super();
-    // makes the ui.react.mount span widen to 1500ms
-    // busy_sleep(1500)
+  function determineProductsEndpoint() {
+    return frontendSlowdown ? '/products-join' : '/products';
   }
 
-  // getProducts handles error responses differently, depending on the browser used
-  async getProducts(frontendSlowdown) {
-    [('/api', '/connect', '/organization')].forEach((endpoint) => {
-      fetch(this.props.backend + endpoint, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }).catch((err) => {
-        // If there's an error, it won't stop the Products http request and page from loading
-        Sentry.captureException(err);
-      });
-    });
+  function fetchUncompressedAsset() {
+    let uc_small_script = document.createElement('script');
+    uc_small_script.async = false;
+    uc_small_script.src =
+      backend +
+      '/compressed_assets/compressed_small_file.js' +
+      '?cacheBuster=' +
+      Math.random();
+    document.body.appendChild(uc_small_script);
 
-    // When triggering a frontend-only slowdown, use the products-join endpoint
-    // because it returns product data with a fast backend response.
-    // Otherwise use the /products endpoint, which provides a slow backend response.
-    const productsEndpoint = frontendSlowdown ? '/products-join' : '/products';
-    console.log(`productsEndpoint: ${productsEndpoint}`);
-    let result = await fetch(this.props.backend + productsEndpoint, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    }).catch((err) => {
-      return { ok: false, status: 500 };
-    });
+    // big uncompressed file
+    let c_big_script = document.createElement('script');
+    c_big_script.async = false;
 
-    if (!result.ok) {
-      Sentry.configureScope(function (scope) {
-        Sentry.setContext('err', {
-          status: result.status,
-          statusText: result.statusText,
-        });
-      });
-      return Promise.reject();
-    } else {
-      const jsonValue = await result.json();
-      return Promise.resolve(jsonValue);
-    }
+    c_big_script.src =
+      backend +
+      '/uncompressed_assets/uncompressed_big_file.js' +
+      '?cacheBuster=' +
+      Math.random();
+    document.body.appendChild(c_big_script);
   }
 
-  async componentDidMount() {
-    var products;
+  // intentionally supposed to be slow
+  function renderProducts(data) {
     try {
-      products = await this.getProducts(this.props.frontendSlowdown);
-
       // Trigger a Sentry 'Performance Issue' in the case of
       // a frontend slowdown
-      if (this.props.frontendSlowdown) {
+      if (frontendSlowdown) {
         // Must bust cache to have force transfer size
         // small compressed file
-        let uc_small_script = document.createElement('script');
-        uc_small_script.async = false;
-        uc_small_script.src =
-          this.props.backend +
-          '/compressed_assets/compressed_small_file.js' +
-          '?cacheBuster=' +
-          Math.random();
-        document.body.appendChild(uc_small_script);
+        fetchUncompressedAsset();
 
-        // big uncompressed file
-        let c_big_script = document.createElement('script');
-        c_big_script.async = false;
-
-        c_big_script.src =
-          this.props.backend +
-          '/uncompressed_assets/uncompressed_big_file.js' +
-          '?cacheBuster=' +
-          Math.random();
-        document.body.appendChild(c_big_script);
-
+        console.log('triggering slow render problem');
         // When triggering a frontend-only slowdown, cause a slow render problem
-        this.props.setProducts(
-          Array(200 / 4)
-            .fill(products.slice(0, 4))
+        setProducts(
+          Array(150) // 150 is arbitrary to make a slow enough render
+            .fill(data.slice(0, 4))
             .flat()
             .map((p, n) => {
               p.id = n;
@@ -98,56 +58,121 @@ class Products extends Component {
             })
         );
       } else {
-        this.props.setProducts(products.slice(0, 4));
+        console.log('setting products quickly');
+        setProducts(data.slice(0, 4));
       }
     } catch (err) {
       Sentry.captureException(new Error('app unable to load products: ' + err));
     }
   }
 
-  render() {
-    const { products } = this.props;
-    return products.length > 0 ? (
-      <div>
-        <ul className="products-list">
-          {products.map((product, i) => {
-            const averageRating = (
-              product.reviews.reduce((a, b) => a + (b['rating'] || 0), 0) /
-              product.reviews.length
-            ).toFixed(1);
+  /*
+    Fetching and setting the products used to be done in an
+    async componentDidMount function. We have (Nov 2023) changed the way of
+    doing this to be non-async, because the async function was getting minified in the
+    relevant browser profiling frame, and this made it hard to showcase profiling.
 
-            let stars = [1, 2, 3, 4, 5].map((index) => {
-              if (index <= averageRating) {
-                return (
-                  <span className="star" key={index}>
-                    &#9733;
-                  </span>
-                );
-              } else {
-                return (
-                  <span className="star" key={index}>
-                    &#9734;
-                  </span>
-                );
-              }
+    Jonas B explained why switching away from async function helped:
+    "I think what you are seeing is unrelated to hooks and probably
+    related to the async keyword + babel transform, hence why it probably got
+    fixed with hooks (no transform on that class method anymore)"
+  */
+  useEffect(() => {
+    // getProducts handles error responses differently, depending on the browser used
+    function getProducts(frontendSlowdown) {
+      let se, customerType, email;
+      Sentry.withScope(function (scope) {
+        [se, customerType] = [scope._tags.se, scope._tags.customerType];
+        email = scope._user.email;
+      });
+
+      [('/api', '/connect', '/organization')].forEach((endpoint) => {
+        fetch(backend + endpoint, {
+          method: 'GET',
+          headers: {
+            se,
+            customerType,
+            email,
+            'Content-Type': 'application/json',
+          },
+        }).catch((err) => {
+          // If there's an error, it won't stop the Products http request and page from loading
+          Sentry.captureException(err);
+        });
+      });
+
+      // When triggering a frontend-only slowdown, use the products-join endpoint
+      // because it returns product data with a fast backend response.
+      // Otherwise use the /products endpoint, which provides a slow backend response.
+      const productsEndpoint = determineProductsEndpoint();
+
+      fetch(backend + productsEndpoint, {
+        method: 'GET',
+        headers: {
+          se,
+          customerType,
+          email,
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((result) => {
+          if (!result.ok) {
+            Sentry.configureScope(function (scope) {
+              Sentry.setContext('err', {
+                status: result.status,
+                statusText: result.statusText,
+              });
             });
+            return Promise.reject();
+          } else {
+            return result.json();
+          }
+        })
+        .then(renderProducts)
+        .catch((err) => {
+          return { ok: false, status: 500 };
+        });
+    }
 
-            return (
-              <ProductCard
-                key={i}
-                product={product}
-                stars={stars}
-              ></ProductCard>
-            );
-          })}
-        </ul>
-      </div>
-    ) : (
-      <div className="loader-container">
-        <Loader type="ThreeDots" color="#f6cfb2" height={150} width={150} />
-      </div>
-    );
-  }
+    getProducts(frontendSlowdown);
+  }, []);
+
+  return products.length > 0 ? (
+    <div>
+      <ul className="products-list">
+        {products.map((product, i) => {
+          const averageRating = (
+            product.reviews.reduce((a, b) => a + (b['rating'] || 0), 0) /
+            product.reviews.length
+          ).toFixed(1);
+
+          let stars = [1, 2, 3, 4, 5].map((index) => {
+            if (index <= averageRating) {
+              return (
+                <span className="star" key={index}>
+                  &#9733;
+                </span>
+              );
+            } else {
+              return (
+                <span className="star" key={index}>
+                  &#9734;
+                </span>
+              );
+            }
+          });
+
+          return (
+            <ProductCard key={i} product={product} stars={stars}></ProductCard>
+          );
+        })}
+      </ul>
+    </div>
+  ) : (
+    <div className="loader-container">
+      <Loader type="ThreeDots" color="#f6cfb2" height={150} width={150} />
+    </div>
+  );
 }
 
 const mapStateToProps = (state, ownProps) => {
