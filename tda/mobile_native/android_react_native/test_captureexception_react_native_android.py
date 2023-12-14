4717ac2af42e70f0eaa17c24e46fcb6a2f150352
Neil Manvar <neil@sentry.io>
2023-12-14 15:20:10 -0800

    fix tests and use better selectors

--- tda/mobile_native/android_react_native/test_captureexception_react_native_android.py
+++ tda/mobile_native/android_react_native/test_captureexception_react_native_android.py
@@ -5,11 +5,11 @@ def test_captureexception_react_native_android(android_react_native_emu_driver):
 
     try:
         # click into list app screen
-        android_react_native_emu_driver.find_element(AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.Button[1]/android.widget.TextView').click()
-        
+        android_react_native_emu_driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'LIST APP').click()
+
         # trigger exception
-        btn = android_react_native_emu_driver.find_element(AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[5]/android.widget.TextView')
+        btn = android_react_native_emu_driver.find_element(AppiumBy.XPATH, '//android.widget.TextView[@text="Capture Exception"]')
         btn.click()
-    
+
     except Exception as err:
         sentry_sdk.capture_exception(err)
