532c8fc2db27a888021bf46cac5f958418a9be5c
Chris Stavitsky <12092849+cstavitsky@users.noreply.github.com>
2023-12-19 13:31:15 -0800

    refactor About component

--- react/src/components/About.js
+++ react/src/components/About.js
@@ -4,6 +4,7 @@ import slugify from '../utils/slugify';
 import * as Sentry from '@sentry/react';
 import './about.css';
 import { isOddReleaseWeek, busy_sleep } from '../utils/time';
+import { useState, useEffect } from 'react';
 
 import Jane from './employees/jane';
 import Lily from './employees/lily';
@@ -14,17 +15,13 @@ import Noah from './employees/noah';
 
 const employees = [Jane, Lily, Keith, Mason, Emma, Noah];
 
-class About extends Component {
-  constructor() {
-    super();
-    // must be inside the constructor to affect LCP, if in componentDidMount() only affects duration
+function About() {
+  useEffect(() => {
     if (!isOddReleaseWeek()) {
       // can't have async sleep in a constructor
       busy_sleep(Math.random(25) + 100);
     }
-  }
 
-  async componentDidMount() {
     // Http requests to make in parallel, so the Transaction has more Spans
     let request1 = fetch(this.props.backend + '/api', {
       method: 'GET',
@@ -40,7 +37,7 @@ class About extends Component {
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/allSettled#browser_compatibility
     // let response = await Promise.allSettled([request1, request2, request3])
 
-    const response = [await request1, await request2, await request3];
+    const response = [request1, request2, request3];
 
     // Error Handling
     response.forEach((r) => {
@@ -55,53 +52,51 @@ class About extends Component {
         );
       }
     });
-  }
-  render() {
-    return (
-      <div className="about-page">
-        <div>
-          <div className="about-info">
-            <h1>About us</h1>
-            <p>
-              Empower Plant is an IoT company determined to keep house plants
-              happy. After reading Michael Pollan’s 2013 New Yorker article
-              (“The Intelligent Plant”), the wife-and-wife founding team
-              invested their life savings in measuring and improving the
-              emotional state of their leafy housemates. Seven years later, the
-              company’s grown from its humble roots in the couples’ backyard
-              greenhouse (converted from a garage) into a Series C-funded San
-              Francisco startup and the world’s most accurate plant mood
-              measurer (“a must-have for any responsible plant parent,”
-              according to Plant Parenthood Magazine). Their original
-              state-of-the-art product is a technological marvel built with a
-              plant-first mindset, and they now offer a range of plant-centric
-              options.
-            </p>
-            <p>
-              Today, the company employs 170 people (and 852 plants) in San
-              Francisco, New York City, Denver, and Kiev.
-            </p>
-          </div>
-        </div>
-        <div>
-          <ul className="employee-list">
-            {employees.map((employee) => {
-              return (
-                <li key={employee.name}>
-                  <Link to={`/employee/${slugify(employee.url)}`}>
-                    <img src={employee.img} alt={`${employee.name}`} />
-                    <h5 className="employee-name" name={employee.name}>
-                      {employee.name}
-                    </h5>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+  }, []);
+
+  return (
+    <div className="about-page">
+      <div>
+        <div className="about-info">
+          <h1>About us</h1>
+          <p>
+            Empower Plant is an IoT company determined to keep house plants
+            happy. After reading Michael Pollan’s 2013 New Yorker article (“The
+            Intelligent Plant”), the wife-and-wife founding team invested their
+            life savings in measuring and improving the emotional state of their
+            leafy housemates. Seven years later, the company’s grown from its
+            humble roots in the couples’ backyard greenhouse (converted from a
+            garage) into a Series C-funded San Francisco startup and the world’s
+            most accurate plant mood measurer (“a must-have for any responsible
+            plant parent,” according to Plant Parenthood Magazine). Their
+            original state-of-the-art product is a technological marvel built
+            with a plant-first mindset, and they now offer a range of
+            plant-centric options.
+          </p>
+          <p>
+            Today, the company employs 170 people (and 852 plants) in San
+            Francisco, New York City, Denver, and Kiev.
+          </p>
         </div>
       </div>
-    );
-  }
+      <div>
+        <ul className="employee-list">
+          {employees.map((employee) => {
+            return (
+              <li key={employee.name}>
+                <Link to={`/employee/${slugify(employee.url)}`}>
+                  <img src={employee.img} alt={`${employee.name}`} />
+                  <h5 className="employee-name" name={employee.name}>
+                    {employee.name}
+                  </h5>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Sentry.withProfiler(About, { name: 'About' });
