d804af1ea7136c6df247dc2107563c85c89d04c2
Chris Stavitsky <12092849+cstavitsky@users.noreply.github.com>
2023-12-19 13:38:07 -0800

    Refactor CompleteError

--- react/src/components/CompleteError.js
+++ react/src/components/CompleteError.js
@@ -1,10 +1,10 @@
-import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './complete.css';
 import * as Sentry from '@sentry/react';
+import { useEffect } from 'react';
 
-class CompleteError extends Component {
-  componentDidMount() {
+function CompleteError() {
+  useEffect(() => {
     window.setTimeout(() => {
       if (sessionStorage.getItem('userFeedback') === 'true') {
         sessionStorage.removeItem('userFeedback');
@@ -19,20 +19,18 @@ class CompleteError extends Component {
         }
       }
     }, 3500);
-  }
+  });
 
-  render() {
-    return (
-      <div className="checkout-container-complete sentry-unmask">
-        <h2>We're having some trouble</h2>
-        <p>
-          We were unable to process your order but will do everything we can to
-          make it right. Please <Link to="/">reach out to us</Link> if you have
-          been charged or have any questions.
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div className="checkout-container-complete sentry-unmask">
+      <h2>We're having some trouble</h2>
+      <p>
+        We were unable to process your order but will do everything we can to
+        make it right. Please <Link to="/">reach out to us</Link> if you have
+        been charged or have any questions.
+      </p>
+    </div>
+  );
 }
 
 export default CompleteError;
