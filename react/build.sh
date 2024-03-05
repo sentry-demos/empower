96e5e4e249ff3d2c989f74bc60ff0bd08c107763
Simon Zhong <simon.zhong@sentry.io>
2024-03-05 14:59:33 -0800

    npm -> yarn

--- react/build.sh
+++ react/build.sh
@@ -7,5 +7,5 @@ set -e # exit immediately if any command exits with a non-zero status
 rm -rf build
 # npm ci does not update minor versions ->
 # (1) less chance of breaking (2) less noise in PR from package-lock.json
-npm ci
-npm run build # defined in 'scripts' in package.json
+yarn
+yarn run build # defined in 'scripts' in package.json
