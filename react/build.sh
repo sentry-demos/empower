675eee36eb5f04bac4dee0fd8c8a6a3ac7867584
Simon Zhong <simon.zhong@sentry.io>
2024-03-05 15:35:22 -0800

    add --frozen-lockfile

--- react/build.sh
+++ react/build.sh
@@ -5,7 +5,7 @@
 set -e # exit immediately if any command exits with a non-zero status
 
 rm -rf build
-# npm ci does not update minor versions ->
-# (1) less chance of breaking (2) less noise in PR from package-lock.json
-yarn
+# npm ci or yarn install --frozen-lockfile does not update minor versions ->
+# (1) less chance of breaking (2) less noise in PR from package-lock.json or yarn.lock
+yarn install --frozen-lockfile
 yarn run build # defined in 'scripts' in package.json
