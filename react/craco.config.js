babadd5f03da2f7bba53a569e5cef7356163491e
Chris Stavitsky <12092849+cstavitsky@users.noreply.github.com>
2024-04-25 10:26:57 -0700

    fix extra plugins key

--- react/craco.config.js
+++ react/craco.config.js
@@ -1,21 +1,17 @@
 const path = require('path');
-const webpack = require("webpack");
+const webpack = require('webpack');
 const { codecovWebpackPlugin } = require('@codecov/webpack-plugin');
 
 module.exports = {
-  /* ... */
-  plugins: [
-    // Put the Codecov webpack plugin after all other plugins
-    webpack: {
-      configure: {
-        plugins: [
-          codecovWebpackPlugin({
-            enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-            bundleName: 'empower',
-            uploadToken: process.env.CODECOV_TOKEN,
-          }),
-        ]
-      }
-    }
-  ],
+  webpack: {
+    configure: {
+      plugins: [
+        codecovWebpackPlugin({
+          enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+          bundleName: 'empower',
+          uploadToken: process.env.CODECOV_TOKEN,
+        }),
+      ],
+    },
+  },
 };
