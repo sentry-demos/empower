45d70cb22b6c234d4b42a27b2ee495884c848fe4
Chris Stavitsky <12092849+cstavitsky@users.noreply.github.com>
2024-04-25 10:23:15 -0700

    fix craco config, maybe

--- react/craco.config.js
+++ react/craco.config.js
@@ -1,14 +1,21 @@
 const path = require('path');
+const webpack = require("webpack");
 const { codecovWebpackPlugin } = require('@codecov/webpack-plugin');
 
 module.exports = {
   /* ... */
   plugins: [
     // Put the Codecov webpack plugin after all other plugins
-    codecovWebpackPlugin({
-      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-      bundleName: 'empower',
-      uploadToken: process.env.CODECOV_TOKEN,
-    }),
+    webpack: {
+      configure: {
+        plugins: [
+          codecovWebpackPlugin({
+            enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+            bundleName: 'empower',
+            uploadToken: process.env.CODECOV_TOKEN,
+          }),
+        ]
+      }
+    }
   ],
 };
