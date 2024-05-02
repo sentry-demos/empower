3ae41311190e4d884be44524a24b8a498f6dadee
Chris Stavitsky <12092849+cstavitsky@users.noreply.github.com>
2024-05-02 16:04:48 -0700

    newline end of file

--- flask/src/test_utils.py
+++ flask/src/test_utils.py
@@ -7,3 +7,4 @@ def test_get_subscription_plan():
     assert get_subscription_plan('monthly') == 'monthly'
     assert get_subscription_plan('annual') == 'annual'
     assert get_subscription_plan('nonexistent_plan') == 'annual'
+    
