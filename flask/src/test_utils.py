9acb86a81ca1e951b192cdd521d4481ad61ba29f
Chris Stavitsky <12092849+cstavitsky@users.noreply.github.com>
2024-05-02 16:57:10 -0700

    merge main branch

--- flask/src/test_utils.py
+++ flask/src/test_utils.py
@@ -2,3 +2,8 @@ from .utils import *
 
 def test():
     assert yuval("abc") == ""
+
+def test_get_subscription_plan():
+    assert get_subscription_plan('monthly') == 'monthly'
+    assert get_subscription_plan('annual') == 'annual'
+    assert get_subscription_plan('nonexistent_plan') == 'annual'
