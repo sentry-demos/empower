0a68dadedcaa51b13c112d6772bf8b0b1bc22002
Hazal Karakus <hazal.karakus@sentry.io>
2025-01-02 16:52:44 +0100

    refactor

--- react/src/components/employees/emma.js
+++ react/src/components/employees/emma.js
@@ -1,11 +1,12 @@
 import img from '../../assets/emma-garcia.jpg';
-import img from '../../assets/cat.jpg';
+import cat from '../../assets/cat.jpg';
 
 const Emma = {
   name: 'Emma Garcia',
   bio: 'Emma is an Engineer. Every day, she speaks with users near and far to help them buy, replace, use, fix, and every so often, turn on Empower Plant’s state-of-the-art plant mood measurer (next time you consider gifting grandma with the latest technology, think of the support reps and buy her some art instead).',
   url: 'emma',
   img,
+  cat
 };
 
 export default Emma;
