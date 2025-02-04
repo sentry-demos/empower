00b22932c18bba3b94da6e412dd73e07fc6060b6
Aidan Landen <aidanlanden22@gmail.com>
2025-02-04 13:42:48 -0500

    enabling debug mode for testing

--- next/sentry.server.config.js
+++ next/sentry.server.config.js
@@ -13,5 +13,5 @@ Sentry.init({
   integrations: [Sentry.prismaIntegration()],
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: true,
 });
