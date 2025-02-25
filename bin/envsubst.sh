07bebda10ee561578344c0296531886165b1e9c0
Kosty Maleyev <kosty.maleyev@sentry.io>
2025-02-25 13:49:51 -0800

    [platform] envsubst.sh

---
+++ bin/envsubst.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Read input from stdin and substitute environment variables
+while IFS= read -r line; do
+  # Use 'eval' with 'echo' to replace environment variables
+  eval "echo \"$line\""
+done
