dd4d07b697b0a4d04af5453d0f48817d3fe6cbcf
Kosty Maleyev <kosty.maleyev@sentry.io>
2025-05-29 15:00:22 -0700

    [tda,react-native] update selectors to match latest app release (#828)
    * [tda,react-native] update selectors to match latest app release
    
    * [tda,react-native] update selectors to match latest app release
    
    * Bum min iOS version to 15.5
    
    * Fix click for demo data and scroll
    
    * fix duplicate element getter
    
    * fix missing ios 15 config
    
    * update android to version 15
    
    * fix ios flow
    
    ---------
    
    Co-authored-by: Krystof Woldrich <krystof.woldrich@sentry.io>

--- tda/mobile_native/ios_react_native/test_checkout_react_native_ios.py
+++ tda/mobile_native/ios_react_native/test_checkout_react_native_ios.py
@@ -5,7 +5,7 @@ import time
 def test_checkout_react_native_ios(ios_react_native_sim_driver):
 
     try:
-        cart_btn = ios_react_native_sim_driver.find_element(AppiumBy.XPATH, '(//XCUIElementTypeOther[@name="Add to cart"])[2]')
+        cart_btn = ios_react_native_sim_driver.find_element(AppiumBy.XPATH, '(//XCUIElementTypeOther[@name="add-to-cart-button-3"])')
         cart_btn.click()
         cart_btn.click()
         cart_btn.click()
@@ -20,7 +20,7 @@ def test_checkout_react_native_ios(ios_react_native_sim_driver):
         if ios_react_native_sim_driver.is_keyboard_shown():
             ios_react_native_sim_driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Return').click()
 
-        ios_react_native_sim_driver.find_element(AppiumBy.XPATH, '(//XCUIElementTypeOther[@name="Place your order"])[2]').click()
+        ios_react_native_sim_driver.find_element(AppiumBy.XPATH, '(//XCUIElementTypeOther[@name="Place your order"])').click()
 
     except Exception as err:
         sentry_sdk.capture_exception(err)
