789cd52b55538871da924e9adbc2e968877301af
Neil Manvar <neil@sentry.io>
2025-06-03 14:30:11 -0700

    fix path to envsubst.sh

--- react/build.sh
+++ react/build.sh
@@ -4,7 +4,7 @@
 
 set -e # exit immediately if any command exits with a non-zero status
 
-envsubst.sh < config-overrides.js.template > config-overrides.js
+../bin/envsubst.sh < config-overrides.js.template > config-overrides.js
 
 export SENTRY_RELEASE=$RELEASE
 
