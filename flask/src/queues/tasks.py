2f98764613db5d7001efd86a403fbe577423f8f8
lukemun <lucasmunro0402@gmail.com>
2025-06-09 17:38:48 -0400

    Add logs to flask - Migrate print to logs.info
    Tested in staging

--- flask/src/queues/tasks.py
+++ flask/src/queues/tasks.py
@@ -1,7 +1,6 @@
 from .email_subscribe import app
 import time, random
-import sentry_sdk
-
+import logging
 
 @app.task(bind=True, default_queue='celery-new-subscriptions')
 def sendEmail(self, email):
@@ -10,7 +9,7 @@ def sendEmail(self, email):
     if x == 0:
         raise Exception("sending email error")
     time.sleep(x)
-    print("Sending email to: " + email)
+    logging.info("Sending email to: " + email)
     return x
   except Exception as e:
       raise self.retry(exc=e, countdown=10, max_retries=5)
