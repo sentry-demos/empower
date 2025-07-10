ea7835dfe629afc2aeaa0f21381d68e17ba8d5fa
Kosty Maleyev <kosty.maleyev@sentry.io>
2025-07-10 11:34:26 -0700

    [tda] fix permissions when deleting venv when created by another user

--- tda/build.sh
+++ tda/build.sh
@@ -2,7 +2,7 @@
 
 set -e 
 #virtualenv --python=`which python3.8` env
-rm -rf env || true
+# old venv directory should already be removed by deploy_tda.sh
 python3.10 -m venv env
 source env/bin/activate 
 pip install -r requirements.txt
