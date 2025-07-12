7a18da2d0c5862083d71b4bceb122b6eb6979f9b
Kosty Maleyev <kosty.maleyev@sentry.io>
2025-07-11 20:06:20 -0700

    [tda] use python3.12 explicitly

--- tda/build.sh
+++ tda/build.sh
@@ -3,6 +3,6 @@
 set -e 
 #virtualenv --python=`which python3.8` env
 # old venv directory should already be removed by deploy_tda.sh
-python3 -m venv env
+python3.12 -m venv env
 source env/bin/activate 
 pip install -r requirements.txt
