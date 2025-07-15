52e1bc176aaacd4b95abef7d237c868249e38bd5
Neil Manvar <neil@sentry.io>
2025-07-15 00:20:44 -0700

    include_local_variables: true

--- ruby-on-rails/config/initializers/sentry.rb
+++ ruby-on-rails/config/initializers/sentry.rb
@@ -2,7 +2,7 @@ Sentry.init do |config|
   config.dsn = ENV['RUBYONRAILS_APP_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  #config.include_local_variables = true
+  config.include_local_variables = true
 
   config.release = ENV['RELEASE']
   config.environment = ENV['RUBYONRAILS_RAILS_ENV'] || "development"
