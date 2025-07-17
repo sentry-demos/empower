71b5be4468aca1748a9d3de5cc4ac7487c109e27
Michael Hoffmann <michael.hoffmann@sentry.io>
2025-07-17 03:22:44 +0200

    Correctly setup Sentry exception reporting

--- laravel-12/bootstrap/app.php
+++ laravel-12/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        Integration::handles($exceptions);
     })->create();
 
