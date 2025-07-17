e0a19aa102b82d86074c702089a8781dd9bdaecb
Kosty Maleyev <kosty.maleyev@sentry.io>
2025-07-17 14:07:18 -0700

    [tda] elevate flagship by bypass CExp, re-attempt checkout, /product-join

--- tda/jobs/desktop_web.sh
+++ tda/jobs/desktop_web.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-BATCH_SIZE=random_20 pytest -s -n 7 --ignore-glob='*_vue.py' desktop_web
+# 0.0 - 0.3 cexp (flask) depening on cexp cycle
+# 0.7 non-cexp
+#   0.42-0.6 flask
+#   0.056-0.08 each of {express,springboot,laravel,rails,aspnetcore}
+# flask total = 0.6 - 0.72
+BATCH_SIZE=random_5_15 pytest -s -n 7 --ignore-glob='*_vue.py' desktop_web
+
