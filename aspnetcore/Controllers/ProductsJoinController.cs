637af7c07fdefd40c9b3880a1574889cf265c0b3
Kosty Maleyev <kosty.maleyev@sentry.io>
2025-07-16 18:21:28 -0700

    [rails,laravel,aspnetcore] add/fix /products-join endpoints

--- aspnetcore/Controllers/ProductsJoinController.cs
+++ aspnetcore/Controllers/ProductsJoinController.cs
@@ -1,9 +1,34 @@
 namespace Empower.Backend.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("products-join")]
 public class ProductsJoinController : ControllerBase
 {
+    private readonly HardwareStoreContext _dbContext;
+
+    public ProductsJoinController(HardwareStoreContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
     [HttpGet]
-    public string Get() => "aspnetcore /products-join";
+    public async Task<IList<Product>> Get()
+    {
+        var products = await GetProductsAsync();
+        
+        return products;
+    }
+
+    private async Task<List<Product>> GetProductsAsync()
+    {
+        var span = SentrySdk.GetSpan()?.StartChild("/products.get_products", "function");
+
+        var products = await _dbContext.Products
+            .Include(e => e.Reviews)
+            .ToListAsync();
+
+        span?.Finish();
+        
+        return products;
+    }
 }
