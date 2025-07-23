53eea9a460cac7f9104f4aead052010ab2a26111
Kosty Maleyev <kosty.maleyev@sentry.io>
2025-07-23 12:22:17 -0700

    [tda] fix ssh through IAP tunnel

--- tda/deploy_tda.sh
+++ tda/deploy_tda.sh
@@ -8,15 +8,21 @@ GCP_PROJECT=sales-engineering-sf
 export CLOUDSDK_CORE_PROJECT=$GCP_PROJECT
 export CLOUDSDK_COMPUTE_ZONE=$HOST_ZONE
 
+function ssh_cmd() {
+    local host=$1
+    shift
+    gcloud compute ssh --tunnel-through-iap $host -- "$@"
+}
+
 function cleanup {
-  echo "NOTE: if ssh check hangs, re-run 'gcloud compute config-ssh; gcloud compute ssh $HOST -- exit' to fix"
+  echo "NOTE: if ssh check hangs, re-run 'gcloud compute config-ssh; gcloud compute ssh --tunnel-through-iap $HOST -- exit' to fix"
 }
 trap cleanup EXIT
 
 echo "Checking ssh connection can be established..."
-gcloud compute ssh $HOST -- -o StrictHostKeyChecking=accept-new exit
+gcloud compute ssh --tunnel-through-iap $HOST -- -o StrictHostKeyChecking=accept-new exit
 if [ $? != "0" ]; then
-  echo "[ERROR] Can't ssh into destination host. Please run 'gcloud compute config-ssh; gcloud compute ssh $HOST -- exit' to fix"
+  echo "[ERROR] Can't ssh into destination host. Please run 'gcloud compute config-ssh; gcloud compute ssh --tunnel-through-iap $HOST -- exit' to fix"
   exit 5 
 fi
 
@@ -40,7 +46,7 @@ fi
 echo "Code copied."
 
 echo "Copying logrotate configuration..."
-gcloud compute ssh $HOST -- 'sudo cp '$DIR'/logrotate.d/tda /etc/logrotate.d/tda && sudo sed -i "s/create 0640 replace_with_user replace_with_user/create 0640 $USER $USER/" /etc/logrotate.d/tda && sudo sed -i "s/su replace_with_user replace_with_user/su $USER $USER/" /etc/logrotate.d/tda'
+ssh_cmd $HOST 'sudo cp '$DIR'/logrotate.d/tda /etc/logrotate.d/tda && sudo sed -i "s/create 0640 replace_with_user replace_with_user/create 0640 $USER $USER/" /etc/logrotate.d/tda && sudo sed -i "s/su replace_with_user replace_with_user/su $USER $USER/" /etc/logrotate.d/tda'
 if [ $? != 0 ]; then
   echo "[ERROR] Failed to copy logrotate configuration to remote host."
   exit 1
@@ -48,7 +54,7 @@ fi
 echo "Logrotate configuration copied."
 
 echo "Testing logrotate configuration..."
-gcloud compute ssh $HOST -- 'sudo logrotate -d /etc/logrotate.d/tda'
+ssh_cmd $HOST 'sudo logrotate -d /etc/logrotate.d/tda'
 if [ $? != 0 ]; then
   echo "[ERROR] Logrotate configuration test failed."
   exit 1
@@ -56,9 +62,9 @@ fi
 echo "Logrotate configuration test passed."
 
 echo "Setting up log directory permissions..."
-gcloud compute ssh $HOST -- 'sudo mkdir -p /var/log && for job in '$DIR'/jobs/*.sh; do job_name=$(basename $job .sh); sudo touch /var/log/tda-$job_name.log; done && sudo chown $USER:$USER /var/log/tda*.log'
-gcloud compute ssh $HOST -- 'sudo touch /var/log/tda-signals.log && sudo chown $USER:$USER /var/log/tda-signals.log'
-gcloud compute ssh $HOST -- 'sudo chown $USER:$USER /var/log/tda*.log*'
+ssh_cmd $HOST 'sudo mkdir -p /var/log && for job in '$DIR'/jobs/*.sh; do job_name=$(basename $job .sh); sudo touch /var/log/tda-$job_name.log; done && sudo chown $USER:$USER /var/log/tda*.log'
+ssh_cmd $HOST 'sudo touch /var/log/tda-signals.log && sudo chown $USER:$USER /var/log/tda-signals.log'
+ssh_cmd $HOST 'sudo chown $USER:$USER /var/log/tda*.log*'
 if [ $? != 0 ]; then
   echo "[ERROR] Failed to set up log directory permissions."
   exit 1
@@ -66,10 +72,10 @@ fi
 echo "Log directory permissions set up."
 
 # setting permissions with rscync doesn't work, leaves 775 instead of 777 (umask?)
-gcloud compute ssh $HOST -- 'find '$DIR' ! -path "*/__pycache__/*" ! -path "*/empower-tda/env/*" ! -path "*/canary.*" -exec sudo chmod 777 {} \;'
+ssh_cmd $HOST 'find '$DIR' ! -path "*/__pycache__/*" ! -path "*/empower-tda/env/*" ! -path "*/canary.*" -exec sudo chmod 777 {} \;'
 
 echo "Cleaning up old virtual environment..."
-gcloud compute ssh $HOST -- 'sudo rm -rf '$DIR'/env'
+ssh_cmd $HOST 'sudo rm -rf '$DIR'/env'
 if [ $? != 0 ]; then
   echo "[ERROR] Failed to clean up old virtual environment."
   exit 1
@@ -78,7 +84,7 @@ echo "Old virtual environment cleaned up."
 
 echo "Installing requirements..."
 # Host must have python3.12 and virtualenv installed
-gcloud compute ssh $HOST -- 'cd '$DIR' && ./build.sh'
+ssh_cmd $HOST 'cd '$DIR' && ./build.sh'
 if [ $? != 0 ]; then
   echo "[ERROR] failed to install requirements on destination host"
   exit 1
@@ -89,10 +95,10 @@ set -e
 #       a situation where nothing is running
 # Side effect would be that then ./stop.sh would stop canaries as well - we don't want that
 echo Killing any currently running TDA processes...
-gcloud compute ssh $HOST -- 'cd '$DIR' && ./stop.sh'
+ssh_cmd $HOST 'cd '$DIR' && ./stop.sh'
 
 echo "Starting TDA..."
-gcloud compute ssh $HOST -- 'cd '$DIR' && ./run.sh'
+ssh_cmd $HOST 'cd '$DIR' && ./run.sh'
 if [ $? != 0 ]; then
   echo "[ERROR] failed to start TDA"
   exit 1
