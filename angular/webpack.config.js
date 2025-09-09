58a5f285690e83de3cb8c93bc013ea0d5d1087f8
Wassim Melakhessou <wassimmelakhessou@LC3HDF6L3Y.local>
2025-09-09 16:54:26 -0700

    Fix webpack config to use dynamic release version
    - Map ANGULAR_APP_RELEASE to process.env.RELEASE instead of hardcoded value
    - Ensures Angular app reports same release version as source map uploads
    - Fixes Sentry source map deobfuscation issue

--- angular/webpack.config.js
+++ angular/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
       // Define environment variables that will be available in the Angular app
       'process.env': {
         'ANGULAR_APP_ENVIRONMENT': JSON.stringify(process.env.ANGULAR_APP_ENVIRONMENT || 'local'),
-        'ANGULAR_APP_RELEASE': JSON.stringify(process.env.ANGULAR_APP_RELEASE || 'dev'),
+        'ANGULAR_APP_RELEASE': JSON.stringify(process.env.RELEASE || 'dev'),
         'ANGULAR_APP_DSN': JSON.stringify(process.env.ANGULAR_APP_DSN || ''),
         'ANGULAR_APP_FLASK_BACKEND': JSON.stringify(process.env.ANGULAR_APP_FLASK_BACKEND || 'http://localhost:8080'),
         'ANGULAR_APP_LARAVEL_BACKEND': JSON.stringify(process.env.ANGULAR_APP_LARAVEL_BACKEND || 'http://localhost:8000'),
