5aad956268999335af236e124331a5e266aeea67
Kosty Maleyev <kosty.maleyev@sentry.io>
2025-09-25 14:36:48 -0700

    fixes to angular <-> new deploy merge

--- angular/run_local.sh
+++ angular/run_local.sh
@@ -3,19 +3,13 @@
 # This is not a standalone script. It is called from ../deploy.sh that
 # sets up the right environment variables and files for it.
 
-if [ "$PORT" == "" ]; then
-  angular_port=4200
-else
-  angular_port="$PORT"
-fi
-
 function cleanup {
-  stop.sh node $angular_port
+  stop.sh node $ANGULAR_LOCAL_PORT
 }
 trap cleanup EXIT
 
 if command -v serve &> /dev/null; then
-    serve -s dist/empower-angular
+    serve -s dist/empower-angular -l $ANGULAR_LOCAL_PORT
 else
-    npx serve -s dist/empower-angular
+    npx serve -s dist/empower-angular -l $ANGULAR_LOCAL_PORT
 fi
