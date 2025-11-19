053d2fa3ac7e0794ff7ae2a530a2958fff8da683
Kosty Maleyev <kosty.maleyev@sentry.io>
2025-11-19 08:53:41 -0800

    [flask] cleanup: delete slow timezone() call, no span for get_iterator

--- flask/src/utils.py
+++ flask/src/utils.py
@@ -19,7 +19,7 @@ weights2 = [0.04, 0.06, 0.1, 0.14, 0.5, 0.16]
 # the 'condition' is either "greater than or equal to" or "less than or equal to"
 # The delay value in seconds is according to a logarithmic distribution of 1 to 10"
 def weighter(condition, hour):
-    current_hour = datetime.now(timezone('America/Los_Angeles')).hour
+    current_hour = datetime.now().hour
     time_to_sleep = choices(times, weights1) if condition(current_hour, hour)  else choices(times, weights2)
     return time_to_sleep[0]
 
@@ -30,11 +30,7 @@ def parseHeaders(keys, headers):
         parsedHeaders[key] = value
     return parsedHeaders
 
-@sentry_sdk.trace
 def get_iterator(n):
-    return _get_iterator(n)
-
-def _get_iterator(n):
     #fibonacci
     if n < 0:
         print("Incorrect input")
@@ -43,7 +39,7 @@ def _get_iterator(n):
     elif n == 1 or n == 2:
         return 1
     else:
-        return _get_iterator(n-1) + _get_iterator(n-2)
+        return get_iterator(n-1) + get_iterator(n-2)
 
 def yuval(text):
     return ""
