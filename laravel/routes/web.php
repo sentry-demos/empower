9d13f339182c9751a7b7d29ad17d8a9b08d69df4
Fabian Schindler <fabian.schindler@sentry.io>
2026-01-12 16:11:48 +0100

    fix(laravel): hooking up outbound requests for laravel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 
 // Web routes matching original Laravel 8.x implementation
 Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products-join', [ProductController::class, 'index']); // Same as products for now
+Route::get('/products-join', [ProductController::class, 'index_joined']); // Same as products for now
 Route::post('/checkout', [ProductController::class, 'checkout']);
 Route::get('/handled', [ProductController::class, 'handled']);
 Route::get('/unhandled', [ProductController::class, 'unhandled']);
