1c471b60224336789f7375a58cb4172960418fd2
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2026-01-12 14:52:14 +0100

    fix(tda): hook up laravel to be accessed by the tda for email subscription (#1154)

--- _tda/desktop_web/test_subscribe.py
+++ _tda/desktop_web/test_subscribe.py
@@ -15,41 +15,42 @@ def test_subscribe(desktop_web_driver, endpoints, batch_size, sleep_length, rand
         # Tag each test iteration with the endpoint
         sentry_sdk.set_tag("endpoint", endpoint)
 
-        # Flask backend is the only one that supports email subscriptions queueing 
-        query_string = {
-          'backend': 'flask'
-        }
+        # Flask and laravel backends are the only ones that support email subscriptions queueing
+        for backend in ['flask', 'laravel']:
+            query_string = {
+                'backend': backend
+            }
 
-        url = endpoint + '?' + urlencode(query_string)
+            url = endpoint + '?' + urlencode(query_string)
 
-        for i in range(batch_size):
-            try:
-                # Navigate to the page
-                desktop_web_driver.get(url)
+            for i in range(batch_size):
+                try:
+                    # Navigate to the page
+                    desktop_web_driver.get(url)
 
-                # Give the page some time to load (increase if needed)
-                time.sleep(sleep_length())
+                    # Give the page some time to load (increase if needed)
+                    time.sleep(sleep_length())
 
-                # Locate the email input field and enter a sample address
-                email_input = desktop_web_driver.find_element(By.CSS_SELECTOR, '#email-subscribe')
-                email_input.send_keys("mySampleEmail@example.com")
+                    # Locate the email input field and enter a sample address
+                    email_input = desktop_web_driver.find_element(By.CSS_SELECTOR, '#email-subscribe')
+                    email_input.send_keys("mySampleEmail@example.com")
 
-                # Locate and click the "Subscribe" button
-                subscribe_btn = desktop_web_driver.find_element(By.CSS_SELECTOR, '.subscribe-button')
-                subscribe_btn.click()
+                    # Locate and click the "Subscribe" button
+                    subscribe_btn = desktop_web_driver.find_element(By.CSS_SELECTOR, '.subscribe-button')
+                    subscribe_btn.click()
 
-                # Wait to allow any subsequent requests to finish
-                time.sleep(sleep_length())
+                    # Wait to allow any subsequent requests to finish
+                    time.sleep(sleep_length())
 
 
-            except NoSuchElementException as err:
-                # If we can't find elements, count it as an abandoned iteration
-                sentry_sdk.capture_exception(err)
+                except NoSuchElementException as err:
+                    # If we can't find elements, count it as an abandoned iteration
+                    sentry_sdk.capture_exception(err)
 
-            except Exception as err:
-                # Catch all other errors
-                sentry_sdk.capture_exception(err)
+                except Exception as err:
+                    # Catch all other errors
+                    sentry_sdk.capture_exception(err)
 
-            # Sleep again at the end of each iteration if needed
+                # Sleep again at the end of each iteration if needed
             time.sleep(sleep_length())
 
