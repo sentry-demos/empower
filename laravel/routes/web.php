54b4aa7fe48569ef27dc7b6c2db9aef8a6623ff5
Fabian Schindler <fabian.schindler@sentry.io>
2026-03-02 14:41:58 +0100

    feat(laravel): enabling Sentry logs and align with Flask backend logs

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\EmailController;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 
@@ -15,15 +16,20 @@ Route::post('/checkout', [ProductController::class, 'checkout']);
 Route::get('/handled', [ProductController::class, 'handled']);
 Route::get('/unhandled', [ProductController::class, 'unhandled']);
 Route::get('/success', function () {
+    Log::info('Received /success endpoint request');
+    Log::info('Completed /success request');
     return 'success';
 });
 Route::get('/api', function () {
+    Log::info('Received /api endpoint request');
     return 'laravel /api';
 });
 Route::get('/organization', function () {
+    Log::info('Received /organization endpoint request');
     return 'laravel /organization';
 });
 Route::get('/connect', function () {
+    Log::info('Received /connect endpoint request');
     return 'laravel /connect';
 });
 Route::get('/debug-sentry', function () {
